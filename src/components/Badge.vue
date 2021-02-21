f30be9e1dfdfbc5e140cb28a91ff7f318283d837
fbraem <franky.braem@gmail.com>
2021-02-21 22:10:22 +0100

    Add Badge

---
+++ src/components/Badge.vue
@@ -0,0 +1,10 @@
+<template>
+  <span class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none rounded-full">
+    <slot></slot>
+  </span>
+</template>
+
+<script>
+export default {
+}
+</script>
