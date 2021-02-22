4330d6cf2aeaeba0203177067d0409b3643d9ddf
fbraem <franky.braem@gmail.com>
2021-02-22 22:05:02 +0100

    Move NewsArchive to the right

--- theme/kwai/index/pages/NewsPage.vue
+++ theme/kwai/index/pages/NewsPage.vue
@@ -13,36 +13,42 @@
       </p>
     </template>
     <AngledSection bg-color="bg-white" text-color="text-white">
-      <OriginalNewsPage>
-        <template #default="{ story }">
-          <p class="pt-6 mb-3 text-sm font-normal text-gray-500">
-            {{ story.publish_date }}
-          </p>
-          <h2 class="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
-            <a
-                v-if="story.has_more"
-                href="#" class="text-gray-900"
-            >
-              {{ story.title }}
-            </a>
-            <span
-                v-else
-                class="text-gray-900"
-            >
-              {{ story.title }}
-            </span>
-          </h2>
-          <p
-              class="mb-4 text-base font-normal text-gray-600"
-              v-html="story.summary"
-          >
-          </p>
-          <button class="bg-red-700 text-white background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" type="button">
-            <i class="fas fa-angle-right"></i> Lees verder ...
-          </button>
-        </template>
-      </OriginalNewsPage>
-      <NewsArchive></NewsArchive>
+      <div class="flex flex-wrap">
+        <div class="w-full md:w-5/12 px-4 mr-auto ml-auto">
+          <OriginalNewsPage>
+            <template #default="{ story }">
+              <p class="pt-6 mb-3 text-sm font-normal text-gray-500">
+                {{ story.publish_date }}
+              </p>
+              <h2 class="mb-2 text-xl font-extrabold leading-snug tracking-tight text-gray-800 md:text-3xl">
+                <a
+                    v-if="story.has_more"
+                    href="#" class="text-gray-900"
+                >
+                  {{ story.title }}
+                </a>
+                <span
+                    v-else
+                    class="text-gray-900"
+                >
+                  {{ story.title }}
+                </span>
+              </h2>
+              <p
+                  class="mb-4 text-base font-normal text-gray-600"
+                  v-html="story.summary"
+              >
+              </p>
+              <button class="bg-red-700 text-white background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1" type="button">
+                <i class="fas fa-angle-right"></i> Lees verder ...
+              </button>
+            </template>
+          </OriginalNewsPage>
+        </div>
+        <div class="w-full md:w-4/12 px-4 mx-auto">
+          <NewsArchive></NewsArchive>
+        </div>
+      </div>
     </AngledSection>
   </Layout>
 </template>
@@ -55,9 +61,11 @@ import Layout from '/@theme/layouts/LandingLayout.vue';
 import OriginalNewsPage from '/src/pages/NewsPage.vue';
 import NewsArchive from '/src/pages/NewsArchive.vue';
 import AngledSection from '/src/components/AngledSection.vue';
+import Badge from '/src/components/Badge.vue';
 
 export default {
   components: {
+    Badge,
     AngledSection,
     Layout,
     OriginalNewsPage,
