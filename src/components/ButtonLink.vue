688d2f9a7e8d20d206c17922212848277a2e73f8
fbraem <franky.braem@gmail.com>
2021-02-23 22:12:42 +0100

    Add ButtonLink

---
+++ src/components/ButtonLink.vue
@@ -0,0 +1,35 @@
+<template>
+  <component :is="tag"
+      class="background-transparent font-bold uppercase px-3 py-1 text-xs outline-none focus:outline-none mr-1 mb-1"
+      @[click]="method"
+      :[to]="route"
+  >
+    <slot></slot>
+  </component>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: {
+    route: {
+      type: String
+    },
+    method: {
+      type: Function
+    }
+  },
+  setup(props) {
+    const tag = computed(() => props.route ? 'router-link' : 'button');
+    const to = computed(() => props.route ? 'to' : null);
+    const click = computed(() => props.method ? 'click' : null);
+
+    return {
+      tag,
+      to,
+      click
+    }
+  }
+}
+</script>
