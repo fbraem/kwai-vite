8e7c1423a4604b2859a973d5e30ce8d8c81aa3a6
fbraem <franky.braem@gmail.com>
2021-02-23 22:14:44 +0100

    Add AngledSection and add section tag

--- theme/kwai/layouts/ContentLayout.vue
+++ theme/kwai/layouts/ContentLayout.vue
@@ -1,25 +1,26 @@
 <template>
   <div>
     <main>
-      <div
-          class="relative pt-16 pb-32 flex content-center items-center justify-center"
-          style="min-height: 75vh;"
-      >
+      <section class="relative block" style="height: 500px;">
         <div
             class="absolute top-0 w-full h-full bg-center bg-cover"
             :style="{ 'background-image': 'url(' + image + ')' }"
         >
-          <span class="w-full h-full absolute opacity-60 bg-black"></span>
+          <span
+              id="blackOverlay"
+              class="w-full h-full absolute opacity-60 bg-black"
+          ></span>
         </div>
-        <div class="container relative mx-auto">
-          <div class="items-center flex flex-wrap">
-            <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-              <slot name="title"></slot>
-            </div>
+      </section>
+      <AngledSection bg-color="bg-gray-300" text-color="text-gray-300">
+      </AngledSection>
+      <section class="relative py-16 bg-gray-300">
+        <div class="container mx-auto px-4">
+          <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-xl rounded-lg -mt-64">
+            <slot></slot>
           </div>
         </div>
-      </div>
-      <slot></slot>
+      </section>
     </main>
   </div>
 </template>
@@ -28,7 +29,9 @@
 </style>
 
 <script>
+import AngledSection from '/src/components/AngledSection.vue';
 export default {
+  components: { AngledSection },
   props: {
     image: {
       type: String
