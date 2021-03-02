4b73f7757f0cae6eaca40df3efe484e8cb297513
fbraem <franky.braem@gmail.com>
2021-03-02 22:29:53 +0100

    Add get

--- src/services/ApplicationService.js
+++ src/services/ApplicationService.js
@@ -1,11 +1,15 @@
 import { useHttp } from '/src/common/useHttp';
 
 function toModel(json) {
-  return json.data.map(d => ({
+  const map = d => ({
     title: d.attributes.title,
     name: d.attributes.name,
     short_description: d.attributes.short_description
-  }));
+  });
+  if ( Array.isArray(json.data)) {
+    return json.data.map(map);
+  }
+  return map(json.data);
 }
 
 const load = () =>
@@ -14,6 +18,12 @@ const load = () =>
     .get()
     .json((json) => toModel(json));
 
+const get = (id) => {
+  let api = useHttp.url(`/application/${id}`);
+  return api.get().json(json => toModel(json));
+}
+
 export const useApplicationService = () => ({
-  load
+  load,
+  get
 });
