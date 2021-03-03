91c2e6f00d1c10a8912274d1d2dd3a77d69ca328
fbraem <franky.braem@gmail.com>
2021-03-03 21:00:52 +0100

    Add collapse/expand to years

--- src/pages/NewsArchive.vue
+++ src/pages/NewsArchive.vue
@@ -1,16 +1,35 @@
 <template>
   <div>
     <slot name="title">
-      <h3 class="text-3xl font-semibold">Archief</h3>
+      <h3 class="text-3xl font-semibold mb-2">Archief</h3>
     </slot>
     <template v-for="year in years">
       <slot name="year" :year="year">
-        <h4 class="text-xl mb-2">{{ year }}</h4>
+        <div class="flex mb-2">
+          <h4 class="flex-grow text-xl hover:cursor-pointer" @click="toggleYear(year)">
+            {{ year }}
+          </h4>
+          <IconLink
+            v-if="showYears[year]"
+            class="hover:bg-gray-200"
+            icon="fas fa-angle-up"
+            :method="() => toggleYear(year)"
+          ></IconLink>
+          <IconLink
+            v-else
+            class="hover:bg-gray-200"
+            icon="fas fa-angle-right"
+            :method="() => toggleYear(year)"
+          ></IconLink>
+        </div>
       </slot>
-      <ul class="mb-2 divide-y divide-gray-300">
+      <ul
+          class="mb-2 divide-y divide-gray-300"
+          v-if="showYears[year]"
+      >
         <li class="py-1" v-for="month in archive[year]">
           <slot name="month" :archive="archive[year]">
-            <div class="relative flex justify-between leading-6">
+            <div class="relative flex leading-6">
               <CoverLink :route="{ name: 'news.archive', params: { year, month: month.month }}">
               </CoverLink>
               <div class="text-blue-600 flex-grow">
@@ -30,17 +49,24 @@
 <script>
 import { useNewsService } from '/src/services/NewsService.js';
 import { months } from '/src/common/useDayJS.js';
+import IconLink from '/src/components/IconLink.vue';
 import useSWRV from 'swrv';
-import { computed } from 'vue';
+import { computed, ref } from 'vue';
 
 import CoverLink from '/src/components/CoverLink.vue';
 import Badge from '/src/components/Badge.vue';
 
 export default {
-  components: { CoverLink, Badge },
+  components: { IconLink, CoverLink, Badge },
   setup() {
     const newsService = useNewsService();
-    const { data, isValidating: loading } = useSWRV('/news/archive', newsService.archive);
+    const { data } = useSWRV(
+      '/news/archive',
+      newsService.archive,
+      {
+        revalidateOnFocus: false
+      }
+    );
 
     const archive = computed(() => {
       if ( data.value ) {
@@ -64,13 +90,35 @@ export default {
       }
     });
 
+    // All years found in the archive
+    const showYears = ref({});
     const years = computed(() => {
-      return Object.keys(archive.value).reverse();
-    })
+      const reversedYears = Object.keys(archive.value).reverse();
+
+      showYears.value = reversedYears.reduce(
+        (a, c) => {
+          a[c] = false;
+          return a;
+        },
+        {}
+      );
+      showYears.value[reversedYears[0]] = true;
+      if (reversedYears.length > 1) {
+        showYears.value[reversedYears[1]] = true;
+      }
+
+      return reversedYears;
+    });
+
+    const toggleYear = (year) => {
+      showYears.value[year] = !showYears.value[year];
+    };
 
     return {
       archive,
-      years
+      years,
+      showYears,
+      toggleYear
     }
   }
 }
