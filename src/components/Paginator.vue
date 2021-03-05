447989d3e9f86ab5301e89cc5cbb5dd07ef667a9
fbraem <franky.braem@gmail.com>
2021-03-05 22:11:44 +0100

    Add Paginator

---
+++ src/components/Paginator.vue
@@ -0,0 +1,164 @@
+<template>
+  <!--
+    Based on paginator from tailwindui:
+    https://tailwindui.com/components/application-ui/navigation/pagination
+  -->
+  <!-- eslint-disable max-len -->
+  <div
+      v-if="pageCount > 1"
+      class="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-200 sm:px-6"
+  >
+    <div class="flex-1 flex justify-between sm:hidden">
+      <button
+         @click="triggerPage(currentPage - 1)"
+         class="relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+         :disabled="currentPage === 1"
+      >
+        {{ previous }}
+      </button>
+      <button
+         @click="triggerPage(currentPage + 1)"
+         class="ml-3 relative inline-flex items-center px-4 py-2 border border-gray-300 text-sm font-medium rounded-md text-gray-700 bg-white hover:text-gray-500"
+         :disabled="currentPage === pageCount"
+      >
+        {{ next }}
+      </button>
+    </div>
+    <div class="hidden sm:flex-1 sm:flex sm:items-center sm:justify-between">
+      <div>
+        <slot name="showing" :data="{ from: offset + 1, to: offset + limit, count }">
+          <p class="text-sm text-gray-700">
+            Showing
+            <span class="font-medium">{{ offset + 1 }}</span>
+            to
+            <span class="font-medium">{{ offset + limit }}</span>
+            of
+            <span class="font-medium">{{ count }}</span>
+            results
+          </p>
+        </slot>
+      </div>
+      <div>
+        <nav class="relative z-0 inline-flex shadow-sm">
+          <span
+              v-if="currentPage === 1"
+              class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500"
+          >
+            <i class="fas fa-chevron-left"></i>
+          </span>
+          <a
+              v-else
+              @click="triggerPage(currentPage - 1)"
+              class="cursor-pointer relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+              aria-label="Previous"
+          >
+            <i class="fas fa-chevron-left"></i>
+          </a>
+          <template v-for="(page, index) in pages" :key="index">
+            <span
+                v-if="page === '...'"
+                class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700"
+            >
+              ...
+            </span>
+            <span
+                v-else-if="page === currentPage"
+                class="-ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-bold text-gray-700"
+            >
+              {{ page }}
+            </span>
+            <a
+                v-else
+                @click="triggerPage(page)"
+                class="cursor-pointer -ml-px relative inline-flex items-center px-4 py-2 border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-700 hover:text-gray-500 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150"
+            >
+              {{ page }}
+            </a>
+          </template>
+          <span
+              v-if="currentPage === pageCount"
+              class="relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500"
+          >
+            <i class="fas fa-chevron-right"></i>
+          </span>
+          <a
+              v-else
+              @click="triggerPage(currentPage + 1)"
+              class="cursor-pointer -ml-px relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-300 bg-white text-sm leading-5 font-medium text-gray-500 hover:text-gray-400 focus:z-10 focus:outline-none focus:border-blue-300 focus:shadow-outline-blue active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150"
+              aria-label="Next"
+          >
+            <i class="fas fa-chevron-right"></i>
+          </a>
+        </nav>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { computed } from 'vue';
+
+export default {
+  props: {
+    limit: Number,
+    offset: Number,
+    count: Number,
+    previous: {
+      type: String,
+      default: 'Previous'
+    },
+    next: {
+      type: String,
+      default: 'Next'
+    }
+  },
+  setup(props, { emit }) {
+    const pageCount = computed(() => Math.ceil(props.count / props.limit));
+
+    const currentPage = computed(() => {
+      let current = 1;
+      for (let offset = 0; offset < props.offset; current++) {
+        offset += props.limit;
+      }
+      return current;
+    });
+
+    const pages = computed(() => {
+      const delta = 3;
+
+      let range = [];
+      for (let i = Math.max(2, currentPage.value - delta);
+          i <= Math.min(pageCount.value - 1,
+              currentPage.value + delta); i++) {
+        range.push(i);
+      }
+      if (currentPage.value - delta > 2) {
+        range.unshift('...');
+      }
+      if (currentPage.value + delta < pageCount.value - 1) {
+        range.push('...');
+      }
+
+      range.unshift(1);
+      range.push(pageCount.value);
+
+      return range;
+    });
+
+    const triggerPage = (newPage) => {
+      let offset = 0;
+      for (let page = 1; page < newPage; page++) {
+        offset += props.limit;
+      }
+      emit('page', offset);
+    };
+
+    return {
+      pageCount,
+      currentPage,
+      pages,
+      triggerPage
+    };
+  }
+};
+</script>
--- src/pages/NewsPage.vue
+++ src/pages/NewsPage.vue
@@ -8,10 +8,31 @@
     <slot v-if="archive" name="archive" :archive="archive">
       <h2>{{ archive.year }} {{ archive.month }}</h2>
     </slot>
-    <div v-for="story in news">
-      <slot :story="story">
-      </slot>
-    </div>
+    <template v-if="news && news.items">
+      <div v-for="story in news.items">
+        <slot :story="story">
+        </slot>
+      </div>
+    </template>
+    <Paginator
+        class="mt-10"
+        :limit="limit"
+        :count="count"
+        :offset="offset"
+        @page="setOffset"
+        previous="Vorige"
+        next="Volgende"
+    >
+      <template #showing :data="data">
+        <p class="text-sm text-gray-700">
+          <span class="font-medium">{{ offset + 1 }}</span>
+          tot
+          <span class="font-medium">{{ offset + limit }}</span>
+          nieuwsberichten van in totaal
+          <span class="font-medium">{{ count }}</span>.
+        </p>
+      </template>
+    </Paginator>
   </div>
 </template>
 
@@ -19,12 +40,13 @@
 import { useNewsService } from '/src/services/NewsService.js';
 import { useApplicationService } from '/src/services/ApplicationService.js';
 import Spinner from '/src/components/Spinner.vue';
+import Paginator from '/src/components/Paginator.vue';
 import useSWRV from 'swrv';
 import { computed, ref } from 'vue';
 import { months } from '/src/common/useDayJS.js';
 
 export default {
-  components: { Spinner },
+  components: { Paginator, Spinner },
   props: {
     year: {
       type: Number
@@ -37,31 +59,33 @@ export default {
     }
   },
   setup(props) {
-    const newsService = useNewsService();
+    const offset = ref(0);
+    const limit = ref(10);
 
-    const paginator = ref({
-      offset: 0,
-      count: 0,
-      limit: 10
-    });
+    const newsService = useNewsService();
 
     const { data: news, isValidating: loading } = useSWRV(
       // the cache key: news_<offset>_<limit>[_<year>][_<month>][_<application>]
       () => [
           'news',
-          paginator.value.offset,
-          paginator.value.limit,
+          offset.value,
+          limit.value,
           props.year, props.month,
           props.app
         ].filter(e => e !== undefined).join('_'),
       () => newsService.load({
         year: props.year,
         month: props.month,
-        offset: paginator.value.offset,
-        limit: paginator.value.limit,
+        offset: offset.value,
+        limit: limit.value,
         application: props.app
       })
     );
+    const count = computed(() => { return news.value?.meta.count ?? 0; });
+    const setOffset = (newOffset) => {
+      console.log(newOffset);
+      offset.value = newOffset;
+    }
 
     const archive = computed(() => {
       if (props.year) {
@@ -88,8 +112,11 @@ export default {
       news,
       loading,
       archive,
-      paginator,
-      application
+      application,
+      count,
+      limit,
+      offset,
+      setOffset
     }
   }
 };
