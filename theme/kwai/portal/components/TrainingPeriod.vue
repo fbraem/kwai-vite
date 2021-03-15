87f1ba17511ab3d3307b56fb7b3213595bc1757a
fbraem <franky.braem@gmail.com>
2021-03-15 22:10:29 +0100

    Add TrainingPeriod

---
+++ theme/kwai/portal/components/TrainingPeriod.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="flex flex-col lg:flex-row flex-wrap">
+    <div class="flex w-full sm:w-1/2">
+      <div>{{ startHour }}</div>
+      <div class="mx-2"><i class="fas fa-arrows-alt-h"></i></div>
+      <div>{{ endHour }}</div>
+    </div>
+    <div class="w-full md:w-1/2">
+      <ul>
+        <li v-for="team in training.teams">
+          <div>{{ team.name }}</div>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+import { formatDate } from '/src/common/useDayJS.js';
+
+export default {
+  props: {
+    training: {
+      type: Object,
+      required: true
+    }
+  },
+  setup(props) {
+    const startHour = formatDate(props.training.start_date, 'HH:mm');
+    const endHour = formatDate(props.training.end_date, 'HH:mm');
+
+    return {
+      startHour,
+      endHour
+    }
+  }
+}
+</script>
