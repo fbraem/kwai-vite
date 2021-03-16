f2fb41c1d5b429751e30909e1d6f68b042f06161
fbraem <franky.braem@gmail.com>
2021-03-16 20:02:19 +0100

    Add more training attributes

--- src/apps/portal/services/TrainingService.js
+++ src/apps/portal/services/TrainingService.js
@@ -1,10 +1,25 @@
 import { useHttp } from '/src/common/useHttp.js';
-import { formatDate } from '/src/common/useDayJS.js';
+import { dateToTimezone, formatDate } from '/src/common/useDayJS.js';
 
 const toModel = (json) => {
   const map = (d) => {
+    let teams = [];
+    if (d.relationships?.teams) {
+      teams = d.relationships.teams.data.map(({id}) => json.included.find((included) => included.type === 'teams' && included.id === id));
+    }
     return {
-      id: d.id
+      id: d.id,
+      title: d.attributes.contents[0].title,
+      summary: d.attributes.contents[0].summary,
+      location: d.attributes.event.location,
+      start_date: dateToTimezone(d.attributes.event.start_date, d.attributes.event.time_zone),
+      end_date: dateToTimezone(d.attributes.event.end_date, d.attributes.event.time_zone),
+      time_zone: d.attributes.event.time_zone,
+      cancelled: d.attributes.event.cancelled,
+      teams: teams.map((team) => ({
+        id: team.id,
+        name: team.attributes.name
+      }))
     };
   }
 
