3518e417c13440aa9f79d5380baa5c217f1917d3
fbraem <franky.braem@gmail.com>
2021-03-18 19:08:41 +0100

    eslint

--- src/apps/portal/composables/useCoaches.js
+++ src/apps/portal/composables/useCoaches.js
@@ -1,12 +1,12 @@
 /* Composable that get all coaches */
 
 import useSWRV from 'swrv';
-import { useCoachesService} from '/src/apps/portal/services/CoachService.js';
+import { useCoachesService } from '/src/apps/portal/services/CoachService.js';
 import { computed } from 'vue';
 
 const service = useCoachesService();
 
-export default function useCoaches(options = {}) {
+export default function useCoaches() {
   const { data: coaches, isValidating: loading, error } = useSWRV(
     '/coaches',
     () => service.load()
@@ -17,5 +17,5 @@ export default function useCoaches(options = {}) {
     count: computed(() => coaches.value?.meta.count),
     loading,
     error
-  }
+  };
 };
