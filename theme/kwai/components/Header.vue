df480ec670c849b2eda23544813ba08a7feb507a
fbraem <franky.braem@gmail.com>
2021-03-20 22:08:51 +0100

    Change text-3xl to text-4xl

--- theme/kwai/components/Header.vue
+++ theme/kwai/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <h2 class="mb-4 text-3xl font-extrabold leading-tight text-gray-900">
+  <h2 class="mb-4 text-4xl font-extrabold leading-tight text-gray-900">
     <slot></slot>
   </h2>
 </template>
