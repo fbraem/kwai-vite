12d90cd7acef593acb236de9e7be0ce3e2664b95
fbraem <franky.braem@gmail.com>
2021-03-22 20:16:33 +0100

    eslint fixes

--- theme/kwai/portal/pages/HomePage.vue
+++ theme/kwai/portal/pages/HomePage.vue
@@ -1,119 +1,126 @@
 <template>
-    <Layout
-      image="/@theme/portal/assets/hero.jpg"
-      :big="true"
+  <Layout
+    image="/@theme/portal/assets/hero.jpg"
+    :big="true"
+  >
+    <template #title>
+      <Hero />
+    </template>
+    <AngledSection
+      bg-color="bg-gray-300"
+      text-color="text-gray-300"
+      class="relative pb-20"
     >
-        <template #title>
-            <Hero/>
-        </template>
-        <AngledSection
-            bg-color="bg-gray-300"
-            text-color="text-gray-300"
-            class="relative pb-20"
+      <div class="container mx-auto px-4">
+        <div class="flex flex-wrap">
+          <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center -mt-32">
+            <ApplicationCard
+              v-if="clubApplication"
+              class="bg-white"
+              :application="clubApplication"
+              icon="fas fa-users"
+              icon-bg-color="bg-red-700"
+            />
+          </div>
+          <div class="pt-6 w-full md:w-4/12 px-4 text-center md:-mt-32">
+            <ApplicationCard
+              v-if="newsApplication"
+              class="bg-white"
+              :application="newsApplication"
+              :route="{ name: 'news' }"
+              icon="fas fa-newspaper"
+              icon-bg-color="bg-red-700"
+            />
+          </div>
+          <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center md:-mt-32">
+            <ApplicationCard
+              v-if="trainingsApplication"
+              class="bg-white"
+              :application="trainingsApplication"
+              :route="{ name: 'trainings' }"
+              icon="fas fa-university"
+              icon-bg-color="bg-red-700"
+            />
+          </div>
+        </div>
+        <div class="flex flex-wrap flex-col lg:flex-row items-center mt-8">
+          <Promotion class="w-full lg:w-5/12" />
+          <Highlight
+            title="10de tornooi Judokwai Kemzeke"
+            image="/@theme/portal/assets/hero.jpg"
+            bg-color="bg-red-700"
+            text-color="text-white"
+            class="w-full mt-4 lg:w-4/12"
+          >
+            <p class="text-md font-light mt-2 text-white">
+              In het weekend van 5-6 oktober organiseert onze club
+              de U11-U13 ontmoeting op zaterdag en de
+              kapoenenontmoeting voor U9 op zondag.
+            </p>
+          </Highlight>
+        </div>
+      </div>
+    </AngledSection>
+    <AngledSection
+      text-color="text-white"
+      bg-color="bg-white"
+    >
+      <div class="lg:mx-36">
+        <div class="pt-6 text-left md:text-center">
+          <h2 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900">
+            Nieuws
+          </h2>
+          <p class="text-lg text-gray-500">
+            Het belangrijkste nieuws van onze club op een rijtje.
+          </p>
+        </div>
+        <div
+          v-if="news"
+          class="flex flex-col divide-y divide-gray-300"
         >
-            <div class="container mx-auto px-4">
-                <div class="flex flex-wrap">
-                    <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center -mt-32">
-                        <ApplicationCard
-                            class="bg-white"
-                            v-if="clubApplication"
-                            :application="clubApplication"
-                            icon="fas fa-users"
-                            icon-bg-color="bg-red-700"
-                        />
-                    </div>
-                    <div class="pt-6 w-full md:w-4/12 px-4 text-center md:-mt-32">
-                        <ApplicationCard
-                            class="bg-white"
-                            v-if="newsApplication"
-                            :application="newsApplication"
-                            :route="{ name: 'news' }"
-                            icon="fas fa-newspaper"
-                            icon-bg-color="bg-red-700"
-                        >
-                        </ApplicationCard>
-                    </div>
-                    <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center md:-mt-32">
-                        <ApplicationCard
-                            class="bg-white"
-                            v-if="trainingsApplication"
-                            :application="trainingsApplication"
-                            :route="{ name: 'trainings' }"
-                            icon="fas fa-university"
-                            icon-bg-color="bg-red-700"
-                        >
-                        </ApplicationCard>
-                    </div>
-                </div>
-                <div class="flex flex-wrap flex-col lg:flex-row items-center mt-8">
-                    <Promotion class="w-full lg:w-5/12"/>
-                    <Highlight
-                        title="10de tornooi Judokwai Kemzeke"
-                        image="/@theme/portal/assets/hero.jpg"
-                        bg-color="bg-red-700"
-                        text-color="text-white"
-                        class="w-full mt-4 lg:w-4/12"
-                    >
-                        <p class="text-md font-light mt-2 text-white">
-                            In het weekend van 5-6 oktober organiseert onze club
-                            de U11-U13 ontmoeting op zaterdag en de
-                            kapoenenontmoeting voor U9 op zondag.
-                        </p>
-                    </Highlight>
-                </div>
-            </div>
-        </AngledSection>
-        <AngledSection text-color="text-white" bg-color="bg-white">
-          <div class="lg:mx-36">
-            <div class="pt-6 text-left md:text-center">
-              <h2 class="mb-2 text-3xl font-extrabold leading-tight text-gray-900">
-                Nieuws
-              </h2>
-              <p class="text-lg text-gray-500">
-                Het belangrijkste nieuws van onze club op een rijtje.
-              </p>
-            </div>
-            <div
-                v-if="news"
-                class="flex flex-col divide-y divide-gray-300"
+          <div
+            v-for="story in news"
+            :key="story.id"
+            class="p-6"
+          >
+            <StoryListItem :story="story" />
+          </div>
+        </div>
+      </div>
+    </AngledSection>
+    <AngledSection
+      text-color="text-gray-700"
+      bg-color="bg-gray-700"
+    >
+      <div class="flex flex-wrap items-center pt-16 mx-auto">
+        <div
+          class="w-full lg:w-1/2 grid grid-cols-1 lg:grid-cols-2 gap-10 mx-auto"
+          style="grid-auto-rows: 1fr;"
+        >
+          <div
+            v-for="(board, index) in information_boards"
+            :key="index"
+          >
+            <Card
+              class="h-full"
+              :title="board.title"
+              :class="board['bg-color'] + ' ' + (board['text-color'] ?? 'text-white')"
             >
-              <div
-                  v-for="story in news"
-                  :key="story.id"
-                  class="p-6"
-              >
-                <StoryListItem :story="story" />
+              <img
+                v-if="board.image"
+                :alt="board.title"
+                :src="board.image"
+                class="max-w-full w-32 mx-auto p-1 bg-white shadow-md rounded-full"
+              />
+              <div class="mt-5 text-opacity-70">
+                <div v-html="board.text"></div>
               </div>
-            </div>
+            </Card>
           </div>
-        </AngledSection>
-        <AngledSection
-            text-color="text-gray-700"
-            bg-color="bg-gray-700"
-        >
-          <div class="flex flex-wrap items-center pt-16 mx-auto">
-            <div class="w-full lg:w-1/2 grid grid-cols-1 lg:grid-cols-2 gap-10 mx-auto" style="grid-auto-rows: 1fr;">
-              <div v-for="board in information_boards">
-                <Card
-                    class="h-full"
-                    :title="board.title"
-                    :class="board['bg-color'] + ' ' + (board['text-color'] ?? 'text-white')"
-                >
-                  <img
-                      v-if="board.image"
-                      :alt="board.title"
-                      :src="board.image"
-                      class="max-w-full w-32 mx-auto p-1 bg-white shadow-md rounded-full"
-                  />
-                  <div class="mt-5 text-opacity-70">
-                    <div v-html="board.text"></div>
-                  </div>
-                </Card>
-              </div>
-            </div>
-          </div>
-        </AngledSection>
-    </Layout>
+        </div>
+      </div>
+    </AngledSection>
+  </Layout>
 </template>
 
 <script>
@@ -122,7 +129,6 @@ import AngledSection from '/src/components/AngledSection.vue';
 import Card from '/src/components/Card.vue';
 import StoryListItem from '/@theme/portal/components/StoryListItem.vue';
 import Hero from '/@theme/portal/components/Hero.vue';
-import ApplicationSection from '/@theme/portal/components/ApplicationSection.vue';
 import ApplicationCard from '/@theme/portal/components/ApplicationCard.vue';
 import Promotion from '/@theme/portal/components/Promotion.vue';
 import Highlight from '/@theme/portal/components/Highlight.vue';
@@ -133,31 +139,30 @@ import usePromotedNews from '/src/apps/portal/composables/usePromotedNews.js';
 import config from '/src/config/config.toml';
 
 export default {
-    components: {
-      StoryListItem,
-        Highlight,
-        Promotion,
-        ApplicationCard,
-        ApplicationSection,
-        AngledSection,
-        Layout,
-        Hero,
-        Card
-    },
-    setup () {
-      const { application: newsApplication } = useApplication('news');
-      const { application: clubApplication } = useApplication('club');
-      const { application: trainingsApplication } = useApplication('trainings');
+  components: {
+    StoryListItem,
+    Highlight,
+    Promotion,
+    ApplicationCard,
+    AngledSection,
+    Layout,
+    Hero,
+    Card
+  },
+  setup() {
+    const { application: newsApplication } = useApplication('news');
+    const { application: clubApplication } = useApplication('club');
+    const { application: trainingsApplication } = useApplication('trainings');
 
-      const { news } = usePromotedNews();
+    const { news } = usePromotedNews();
 
-      return {
-        newsApplication,
-        clubApplication,
-        trainingsApplication,
-        news,
-        information_boards: config.website.information_boards
-      };
-    }
+    return {
+      newsApplication,
+      clubApplication,
+      trainingsApplication,
+      news,
+      information_boards: config.website.information_boards
+    };
+  }
 };
 </script>
