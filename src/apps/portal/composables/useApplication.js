560c3676a032e9fef3e0b05ffcc507549692d54b
fbraem <franky.braem@gmail.com>
2021-04-02 19:48:32 +0200

    Allow id as argument and make the key dependent on applications

--- src/apps/portal/composables/useApplication.js
+++ src/apps/portal/composables/useApplication.js
@@ -1,18 +1,43 @@
 /* Composable for getting a specific application */
 
 import useApplications from '/src/apps/portal/composables/useApplications.js';
-import { computed } from 'vue';
+import { isRef } from 'vue';
+import useSWRV from 'swrv';
 
-export default function useApplication(name) {
+export default function useApplication({
+  name, id
+}) {
   const { applications } = useApplications();
-
-  const application = computed(() => {
-    if (applications.value) {
-      return applications.value.find(
-        (d) => d.name === name
-      );
+  const { data: application } = useSWRV(
+    () => {
+      if (!applications.value) return null;
+      if (isRef(id)) {
+        if (id.value) {
+          return `/application/${id.value}`;
+        }
+        return null;
+      }
+      if (id) return `/application/${id}`;
+      if (name) return `/application/${name}`;
+      return null;
+    },
+    () => {
+      let application = null;
+      if (name) {
+        application = applications.value.find(
+          (d) => d.name === name
+        );
+      }
+      if (id) {
+        application = applications.value.find(
+          (d) => {
+            return d.id === (isRef(id) ? id.value : id).toString();
+          }
+        );
+      }
+      return application;
     }
-  });
+  );
 
   return {
     application
