bf4b3869b42704444da8b3f64baf3ffa846751b5
fbraem <franky.braem@gmail.com>
2021-04-29 19:33:10 +0200

    Update height of hero and center the content

--- theme/kwai/layouts/LandingLayout.vue
+++ theme/kwai/layouts/LandingLayout.vue
@@ -1,14 +1,12 @@
 <template>
   <div>
     <main>
-      <div
-          class="relative pt-16 pb-24 xl:pb-32 2xl:pb-64 flex content-center items-center justify-center"
-      >
+      <div class="relative min-h-1/2 flex items-center">
         <div
             class="absolute top-0 w-full h-full bg-center md:bg-top bg-cover"
             :style="{ 'background-image': 'url(' + image + ')' }"
         >
-          <span class="w-full h-full absolute opacity-60 bg-black"></span>
+          <span class="w-full h-full absolute opacity-50 bg-black"></span>
         </div>
         <div class="container relative mx-auto">
           <div class="items-center flex flex-wrap">
