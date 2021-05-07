ce6838e89d6de27cd0553e4140613093a7cfe621
fbraem <franky.braem@gmail.com>
2021-05-07 19:21:04 +0200

    A toolbar that works together with the sidebar

---
+++ src/components/sidebar/Toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header
+    class="flex justify-between items-center py-4 px-6 bg-red-700"
+  >
+    <div class="flex items-center">
+      <button
+        class="text-white focus:outline-none lg:hidden"
+        @click="isOpen = true"
+      >
+        <i class="fas fa-bars" />
+      </button>
+    </div>
+
+    <div class="flex items-center space-x-2">
+      <slot></slot>
+    </div>
+  </header>
+</template>
+
+<script>
+import { useSidebar } from '/src/components/sidebar/useSidebar.js';
+
+export default {
+  setup() {
+    const { isOpen } = useSidebar();
+
+    return {
+      isOpen
+    };
+  }
+};
+</script>
