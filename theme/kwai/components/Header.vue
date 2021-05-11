193f0c53bed33eacb1b1a8651ac5adcd9cb38361
fbraem <franky.braem@gmail.com>
2021-05-11 22:13:34 +0200

    eslint

--- theme/kwai/components/Header.vue
+++ theme/kwai/components/Header.vue
@@ -1,10 +1,10 @@
 <template>
   <h2 class="mb-4 text-4xl font-extrabold leading-tight text-gray-900">
-    <slot></slot>
+    <slot />
   </h2>
 </template>
 
 <script>
 export default {
-}
+};
 </script>
