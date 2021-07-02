b767168b87a1896236241a433b20aa8a76121bd5
fbraem <franky.braem@gmail.com>
2021-07-02 18:39:08 +0200

    Remove overflow-hidden because it hides popovers

--- src/components/form/Form.vue
+++ src/components/form/Form.vue
@@ -9,7 +9,7 @@
       </div>
     </div>
     <div class="mt-5 md:mt-0 md:col-span-2">
-      <div class="shadow sm:rounded-md sm:overflow-hidden">
+      <div class="shadow sm:rounded-md">
         <div
           :class="formBgColor"
           class="px-4 py-5 space-y-6 sm:p-6"
