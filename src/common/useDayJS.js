134f0b6192029a47ec0830ec685228a3805c0091
fbraem <franky.braem@gmail.com>
2021-07-26 18:37:38 +0200

    Add customParseFormat

--- src/common/useDayJS.js
+++ src/common/useDayJS.js
@@ -10,11 +10,13 @@ import localeData from 'dayjs/plugin/localeData';
 dayjs.extend(localeData);
 import weekday from 'dayjs/plugin/weekday';
 dayjs.extend(weekday);
+import customParseFormat from 'dayjs/plugin/customParseFormat';
+dayjs.extend(customParseFormat);
 
 // dayjs.utc();
 
-import locale_en from 'dayjs/locale/en';
-import locale_nl from 'dayjs/locale/nl';
+import 'dayjs/locale/en';
+import 'dayjs/locale/nl';
 dayjs.locale('nl');
 
 export const createDateFromFormat = (date, format) => dayjs(date, format ?? 'YYYY-MM-DD');
@@ -28,3 +30,8 @@ export const months = () => dayjs.months();
 export const daysInMonths = () => dayjs.daysInMonth();
 export const startOfMonth = (date) => date.startOf('M');
 export const weekdays = () => dayjs.weekdays(true);
+export const monthsShort = () => dayjs.monthsShort();
+export const weekdaysShort = () => dayjs.weekdaysShort(true);
+// TODO: Remove all functions above
+
+export default dayjs;
