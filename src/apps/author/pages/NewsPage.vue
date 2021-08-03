63c87f6d7fb28dbdc68b2977b9c04f69c79f3d84
fbraem <franky.braem@gmail.com>
2021-08-03 19:08:04 +0200

    Add color to table header

--- src/apps/author/pages/NewsPage.vue
+++ src/apps/author/pages/NewsPage.vue
@@ -1,6 +1,8 @@
 <template>
   <div class="container mx-auto p-4">
-    <h1 class="text-3xl font-medium">Nieuws</h1>
+    <h1 class="text-3xl font-medium">
+      Nieuws
+    </h1>
     <div class="flex flex-wrap -mx-6 my-3">
       <div class="w-full px-6 sm:w-1/2 xl:w-1/3">
         <StatCard
@@ -9,64 +11,69 @@
           class="bg-white"
         >
           <div class="mx-5">
-            <h4 class="text-2xl font-semibold text-gray-700">{{ count }}</h4>
-            <div class="text-gray-500">Berichten</div>
+            <h4 class="text-2xl font-semibold text-gray-700">
+              {{ count }}
+            </h4>
+            <div class="text-gray-500">
+              Berichten
+            </div>
           </div>
         </StatCard>
       </div>
     </div>
-    <table class="w-full bg-white rounded-lg">
-      <thead class="border-b">
-        <tr>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Titel
-          </th>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Application
-          </th>
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            Status
-          </th>
-          <th class="px-6 py-3 text-left">
-          </th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr
-          v-for="story in stories"
-          :key="story.id"
-          :class="{ 'bg-gray-100': !story.enabled }"
-        >
-          <td class="px-6 py-3 border-b flex items-center justify-between">
-            <span class="font-medium">
-              {{ story.title }}
-            </span>
-            <i
-              v-if="story.promotion"
-              class="ml-2 fas fa-bullhorn text-gray-500"
-            />
-          </td>
-          <td class="px-6 py-3 border-b">
-            {{ story.application.title }}
-          </td>
-          <td class="px-6 py-3 border-b text-center">
-            <i
-              v-if="story.enabled"
-              class="fas fa-check text-green-500"
-            />
-            <i
-              v-else
-              class="fas fa-times text-red-500"
-            />
-          </td>
-          <td class="px-6 py-3 border-b text-gray-700">
-            <router-link :to="{ name: 'author.news.edit', params: { id: story.id } }">
-              <i class="fas fa-edit" />
-            </router-link>
-          </td>
-        </tr>
-      </tbody>
-    </table>
+    <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg bg-white">
+      <table class="min-w-full">
+        <thead>
+          <tr>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
+              Titel
+            </th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
+              Application
+            </th>
+            <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
+              Status
+            </th>
+            <th class="px-6 py-3 text-left bg-gray-50" />
+          </tr>
+        </thead>
+        <tbody>
+          <tr
+            v-for="story in stories"
+            :key="story.id"
+            :class="{ 'bg-gray-100': !story.enabled }"
+          >
+            <td class="px-6 py-3 border-b flex items-center justify-between">
+              <span class="font-medium">
+                {{ story.title }}
+              </span>
+              <i
+                v-if="story.promotion"
+                class="ml-2 fas fa-bullhorn text-gray-500"
+              />
+            </td>
+            <td class="px-6 py-3 border-b">
+              {{ story.application.title }}
+            </td>
+            <td class="px-6 py-3 border-b text-center">
+              <i
+                v-if="story.enabled"
+                class="fas fa-check text-green-500"
+              />
+              <i
+                v-else
+                class="fas fa-times text-red-500"
+              />
+            </td>
+            <td class="px-6 py-3 border-b text-gray-700">
+              <router-link :to="{ name: 'author.news.edit', params: { id: story.id } }">
+                <i class="fas fa-edit" />
+              </router-link>
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
     <Paginator
       :pagination="pagination"
       class="bg-white mt-2"
