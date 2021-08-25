6e99257a72fb1af19b15e190bb65adc37a327f03
fbraem <franky.braem@gmail.com>
2021-08-25 22:21:39 +0200

    Mobile first

--- theme/kwai/apps/portal/pages/EventsPage.vue
+++ theme/kwai/apps/portal/pages/EventsPage.vue
@@ -3,41 +3,71 @@
     <template #title>
       Activiteiten
     </template>
-    <section class="bg-gray-200 py-8 px-12">
-      <Header class="flex flex-row py-4 justify-center space-x-6">
-        <div>
-          <ButtonLink
-            class="bg-red-600 text-white"
-            :route="{ name: 'portal.events', params: { ...previous } }"
-          >
-            <i class="fa fa-angle-left mr-2" />Vorige maand
-          </ButtonLink>
+    <section class="bg-gray-200 py-4 sm:py-8 px-4 sm:px-12">
+      <div class="mb-4">
+        <div class="flex flex-row justify-center space-x-6 py-4">
+          <div>
+            <ButtonLink
+              class="bg-red-600 text-white"
+              :route="{ name: 'portal.events', params: { ...previous } }"
+            >
+              <i class="fa fa-angle-left sm:mr-2" />
+              <span class="hidden sm:inline">
+                Vorige maand
+              </span>
+            </ButtonLink>
+          </div>
+          <h2 class="flex-grow text-2xl md:text-3xl font-bold uppercase text-center">
+            {{ monthName }} {{ year }}
+          </h2>
+          <div>
+            <ButtonLink
+              class="bg-red-600 text-white"
+              :route="{ name: 'portal.events', params: { ...next } }"
+            >
+              <span class="hidden sm:inline">
+                Volgende maand
+              </span>
+              <i class="fa fa-angle-right sm:ml-2" />
+            </ButtonLink>
+          </div>
         </div>
-        <div class="uppercase">
-          {{ monthName }} {{ year }}
+        <div
+          v-if="store.count === 0"
+          class="text-center text-sm"
+        >
+          Er zijn geen activiteiten gepland deze maand.
         </div>
-        <div>
-          <ButtonLink
-            class="bg-red-600 text-white"
-            :route="{ name: 'portal.events', params: { ...next } }"
-          >
-            Volgende maand<i class="fa fa-angle-right ml-2" />
-          </ButtonLink>
+        <div
+          v-else-if="store.count === 1"
+          class="text-center text-sm"
+        >
+          Er is 1 activiteit gepland deze maand.
         </div>
-      </Header>
+        <div
+          v-else-if="store.count > 1"
+          class="text-center text-sm"
+        >
+          Er zijn
+          <span class="font-bold">
+            {{ store.count }}
+          </span>
+          activiteiten gepland deze maand.
+        </div>
+      </div>
       <div class="container mx-auto bg-white p-4 divide-y divide-gray-300">
         <div
           v-for="(date, index) in days"
           :key="index"
-          class="flex items-center p-4"
+          class="grid grid-cols-8 grid-flow-row p-2 sm:p-4"
         >
-          <div class="flex justify-center w-20">
-            <div class="w-1/2 text-center font-bold bg-red-600 text-white p-2">
+          <div>
+            <div class="text-center font-bold bg-red-600 text-white w-8 p-1 sm:w-10 sm:p-2">
               {{ date.day }}
             </div>
           </div>
-          <div class="px-2">
-            <h1 class="text-sm mb-3">
+          <div class="col-span-7 px-2">
+            <h1 class="text-xs sm:text-sm mb-3">
               {{ date.formatted }}
             </h1>
             <div
@@ -46,15 +76,22 @@
             >
               Geen activiteiten
             </div>
-            <div class="divide-y divide-gray-300">
+          </div>
+          <div class="col-span-8">
+            <div class="divide-y divide-gray-300 divide-dotted">
               <div
                 v-for="event in date.events"
                 :key="event.id"
-                class="py-2"
+                class="py-2 px-2"
               >
-                <div class="flex flex-row space-x-2">
-                  <div>
+                <div class="flex flex-row flex-wrap sm:flex-nowrap">
+                  <div class="sm:order-first flex-shrink-0 mr-2">
                     {{ event.start_date.format('HH:mm') }} - {{ event.end_date.format('HH:mm') }}
+                  </div>
+                  <div class="sm:order-last flex flex-wrap space-x-1 sm:ml-2">
+                    <div>
+                      <Badge class="bg-red-600 text-white font-bold text-xs">#Training</Badge>
+                    </div>
                   </div>
                   <div class="font-bold">
                     {{ event.title }}
@@ -62,7 +99,7 @@
                 </div>
                 <div
                   v-if="event.summary"
-                  class="text-xs"
+                  class="text-xs sm:text-sm"
                 >
                   {{ event.summary }}
                 </div>
