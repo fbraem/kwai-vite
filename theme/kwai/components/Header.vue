2049b0c36cbd9010a6b0cd9f18ccc0682466f128
fbraem <franky.braem@gmail.com>
2021-09-04 21:15:04 +0200

    Add div wrapper

--- theme/kwai/components/Header.vue
+++ theme/kwai/components/Header.vue
@@ -1,7 +1,9 @@
 <template>
-  <h2 class="mb-4 text-2xl sm:text-3xl font-extrabold leading-tight text-gray-900">
-    <slot />
-  </h2>
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8 text-center">
+    <h2 class="mb-4 text-2xl sm:text-3xl font-extrabold leading-tight text-gray-900">
+      <slot />
+    </h2>
+  </div>
 </template>
 
 <script>
