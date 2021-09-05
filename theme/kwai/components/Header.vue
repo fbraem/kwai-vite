565216f35a1754e50811221867fd8ef03348a821
fbraem <franky.braem@gmail.com>
2021-09-05 22:44:43 +0200

    Smaller pb

--- theme/kwai/components/Header.vue
+++ theme/kwai/components/Header.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-8 text-center">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-4 text-center">
     <h2 class="mb-4 text-2xl sm:text-3xl font-extrabold leading-tight text-gray-900">
       <slot />
     </h2>
