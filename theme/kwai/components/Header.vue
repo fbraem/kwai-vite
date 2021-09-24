3fde8b9542680cf0714844bb1672f35a1e687a6e
fbraem <franky.braem@gmail.com>
2021-09-24 21:58:33 +0200

    Change font weight, remove div

--- theme/kwai/components/Header.vue
+++ theme/kwai/components/Header.vue
@@ -1,9 +1,7 @@
 <template>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pb-4 text-center">
-    <h2 class="mb-4 text-2xl sm:text-3xl font-extrabold leading-tight text-gray-900">
-      <slot />
-    </h2>
-  </div>
+  <h2 class="text-2xl sm:text-3xl font-medium leading-tight text-gray-900">
+    <slot />
+  </h2>
 </template>
 
 <script>
