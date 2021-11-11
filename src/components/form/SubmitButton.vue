707570bd1462847dbfc671ff2b0fa5f35b4e84bb
fbraem <franky.braem@gmail.com>
2021-11-11 19:23:59 +0100

    bind $attrs

--- src/components/form/SubmitButton.vue
+++ src/components/form/SubmitButton.vue
@@ -1,5 +1,6 @@
 <template>
   <button
+    v-bind="$attrs"
     class="text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
     type="button"
     @click="$emit('click')"
