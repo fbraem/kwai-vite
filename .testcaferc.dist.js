bbe5b3664b1b58dff8c083c5f0aa76419375c2ea
fbraem <franky.braem@gmail.com>
2021-11-13 18:52:49 +0100

    Add config for testcafe

---
+++ .testcaferc.dist.js
@@ -0,0 +1,10 @@
+// Configure testcafe by renaming this file to .testcaferc.js and change the
+// required configuration variables.
+// Warning: Don't put .testcaferc.js into source control!
+
+module.exports = {
+  userVariables: {
+    login_email: '',
+    login_password: ''
+  }
+};
