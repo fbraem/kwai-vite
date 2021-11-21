42b447ef3184fad4f4193a437ba3b572cbe4855a
fbraem <franky.braem@gmail.com>
2021-11-21 12:51:20 +0100

    Center the title

--- theme/kwai/apps/portal/components/Promotion.vue
+++ theme/kwai/apps/portal/components/Promotion.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="px-4 mr-auto ml-auto">
-        <h3 class="text-3xl mb-2 font-semibold leading-normal">
+        <h3 class="text-center text-3xl mb-2 font-semibold leading-normal">
             {{ title }}
         </h3>
         <div class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
