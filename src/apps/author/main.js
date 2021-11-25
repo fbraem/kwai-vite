dffbefc1572b58b8dec4bd7540c5e8b97857ddfa
fbraem <franky.braem@gmail.com>
2021-11-25 21:43:11 +0100

    Add pinia

--- src/apps/author/main.js
+++ src/apps/author/main.js
@@ -2,6 +2,9 @@ import { createApp } from 'vue';
 import App from '/src/App.vue';
 const app = createApp(App);
 
+import { createPinia } from 'pinia';
+app.use(createPinia());
+
 import { abilitiesPlugin } from '@casl/vue';
 import { useAbility } from '/src/common/useAbility.js';
 app.use(abilitiesPlugin, useAbility());
