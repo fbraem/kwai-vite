c25339606c9d39cbf30adfc20162eb03520c16c9
fbraem <franky.braem@gmail.com>
2021-12-14 21:42:40 +0100

    eslint

--- theme/kwai/apps/portal/components/Promotion.vue
+++ theme/kwai/apps/portal/components/Promotion.vue
@@ -1,24 +1,23 @@
 <template>
-    <div class="px-4 mr-auto ml-auto">
-        <h3 class="text-center text-3xl mb-2 font-semibold leading-normal">
-            {{ title }}
-        </h3>
-        <div class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
-            <div v-html="text">
-            </div>
-        </div>
+  <div class="px-4 mx-auto">
+    <h3 class="text-center text-3xl mb-2 font-semibold leading-normal">
+      {{ title }}
+    </h3>
+    <div class="text-lg font-light leading-relaxed mt-4 mb-4 text-gray-700">
+      <div v-html="text" />
     </div>
+  </div>
 </template>
 
 <script>
 import { website } from '/@config';
 
 export default {
-    setup() {
-        return {
-            title: website.promotion.title,
-            text: website.promotion.text
-        }
-    }
-}
+  setup() {
+    return {
+      title: website.promotion.title,
+      text: website.promotion.text
+    };
+  }
+};
 </script>
