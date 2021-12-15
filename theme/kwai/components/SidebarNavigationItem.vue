88f3810dfb3a491dd075251c59918b38cfaac166
fbraem <franky.braem@gmail.com>
2021-12-15 21:36:20 +0100

    Add id main to use to scroll back to the top when a sidebar item is clicked

--- theme/kwai/components/SidebarNavigationItem.vue
+++ theme/kwai/components/SidebarNavigationItem.vue
@@ -8,7 +8,7 @@
       :href="href"
       class="flex items-center duration-200 mt-4 py-2 px-6 border-l-4 hover:cursor-pointer"
       :class="[isExactActive ? activeClass : inactiveClass]"
-      @click="navigate"
+      @click="go(navigate);"
     >
       {{ route.meta.title ?? 'meta.title missing' }}
     </a>
@@ -17,6 +17,13 @@
 
 <script>
 import { ref } from 'vue';
+
+const scrollToTop = () => {
+  const top = document.querySelector('#main');
+  if (top) {
+    top.scrollTo(0, 0);
+  }
+};
 
 export default {
   props: {
@@ -33,9 +40,15 @@ export default {
       'border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100'
     );
 
+    const go = async(navigate) => {
+      await navigate();
+      scrollToTop();
+    };
+
     return {
       activeClass,
-      inactiveClass
+      inactiveClass,
+      go
     };
   }
 };
--- theme/kwai/layouts/DefaultLayout.vue
+++ theme/kwai/layouts/DefaultLayout.vue
@@ -3,7 +3,10 @@
     <router-view name="sidebar" />
     <div class="flex-1 flex flex-col overflow-hidden">
       <router-view name="toolbar" />
-      <main class="flex-1 overflow-x-hidden overflow-y-auto">
+      <main
+        id="main"
+        class="flex-1 overflow-x-hidden overflow-y-auto"
+      >
         <slot />
         <router-view name="footer" />
       </main>
