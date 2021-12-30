7e083f682500fa1e97be71ecba258cf4791658e3
fbraem <franky.braem@gmail.com>
2021-12-30 20:21:46 +0100

    Add ColorIcon.vue

---
+++ src/components/ColorIcon.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="inline-flex items-center justify-center h-6 w-6 rounded">
+    <slot>
+      <i :class="icon" />
+    </slot>
+  </div>
+</template>
+
+<script setup>
+  defineProps({
+    icon: {
+      type: String,
+      required: false,
+      default: null
+    }
+  });
+</script>
