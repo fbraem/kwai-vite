9ab70f05785e8a0d04135fcc0fca2c534a88791c
fbraem <franky.braem@gmail.com>
2021-12-31 17:24:00 +0100

    Add get/save

--- src/apps/admin/stores/userStore.js
+++ src/apps/admin/stores/userStore.js
@@ -2,17 +2,23 @@ import { defineStore } from 'pinia';
 import { watch } from 'vue';
 import { useRequest } from 'vue-request';
 import { useHttpApi } from '/src/common/useHttp.js';
-import dayjs from '../../../common/useDayJS.js';
+import dayjs from '/src/common/useDayJS.js';
 
 const toUserModel = json => {
   const map = d => {
     return {
       id: d.id,
+      type: d.type,
       username: d.attributes.username,
+      first_name: d.attributes.first_name,
+      last_name: d.attributes.last_name,
       email: d.attributes.email,
       last_login: d.attributes.last_login ? dayjs.utc(d.attributes.last_login, 'YYYY-MM-DD HH:mm:ss').format('L LTS') : null,
       last_unsuccessful_login: d.attributes.last_unsuccessful_login ? dayjs.utc(d.attributes.last_unsuccessful_login, 'YYYY-MM-DD HH:mm:ss').format('L LTS') : null,
-      revoked: d.attributes.revoked
+      revoked: d.attributes.revoked,
+      remark: d.attributes.remark,
+      owner: d.attributes.owner,
+      uuid: d.attributes.uuid
     };
   };
 
@@ -29,6 +35,30 @@ export const useUserStore = defineStore('admin.users', {
     users: []
   }),
   actions: {
+    get(uuid) {
+      const { data, loading, error } = useRequest(
+        () => useHttpApi
+          .url('/users/')
+          .url(uuid)
+          .get()
+          .json(),
+        {
+          cacheKey: `/admin/users/${uuid}`,
+          errorRetryCount: 5,
+        }
+      );
+      watch(
+        data,
+        json => {
+          this.user = toUserModel(json);
+        }
+      );
+
+      return {
+        loading,
+        error
+      };
+    },
     load() {
       const { data, loading, error } = useRequest(
         () => useHttpApi.url('/users/accounts').get().json(),
@@ -50,6 +80,26 @@ export const useUserStore = defineStore('admin.users', {
         loading,
         error
       };
+    },
+    save(user) {
+      const payload = {
+        data: {
+          type: 'users',
+          attributes: {
+            first_name: user.first_name,
+            last_name: user.last_name,
+            remark: user.remark
+          }
+        }
+      };
+      return useHttpApi
+        .url('/users')
+        .url(user.uuid)
+        .json(payload)
+        .patch()
+        .json()
+        .then(json => { this.user = toUserModel(json); })
+      ;
     }
   }
 });
