14914d67938c5de6ce4f5e98618a21f7b3e49bd5
fbraem <franky.braem@gmail.com>
2021-12-31 17:30:47 +0100

    Restyle and use script setup

--- src/apps/admin/pages/UsersPage.vue
+++ src/apps/admin/pages/UsersPage.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <PageSectionTitle>
-      Gebruikers
+      {{ $route.meta.title }}
     </PageSectionTitle>
     <PageSection>
       <div class="w-full sm:w-1/2 xl:w-1/3">
@@ -19,92 +19,128 @@
       </div>
     </PageSection>
     <PageSection>
-      <div class="inline-block min-w-full overflow-hidden align-middle border-b border-gray-200 shadow sm:rounded-lg bg-white">
-        <table class="min-w-full">
-          <thead>
-            <tr>
-              <th class="px-6 py-3 w-10 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
-                <i
-                  v-if="loading"
-                  class="fas fa-spin fa-spinner"
-                />
-              </th>
-              <th class="pr-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
-                Naam
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
-                <i class="fas fa-envelope mr-1" /> Email
-              </th>
-              <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 bg-gray-50 uppercase tracking-wider">
-                <i class="fas fa-envelope mr-1" /> Login
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr
-              v-for="user in users"
-              :key="user.id"
-            >
-              <td class="px-6 py-3 border-b w-10">
+      <div class="space-y-10">
+        <div
+          v-for="user in users"
+          :key="user.id"
+        >
+          <div class="shadow overflow-hidden rounded-lg">
+            <div class="bg-gray-200 px-4 py-5 sm:px-6">
+              <h3 class="text-lg leading-6 font-medium text-gray-900">
                 <i
                   v-show="!user.revoked"
-                  class="fas fa-user"
+                  class="fas fa-user mr-2"
                 />
                 <i
                   v-show="user.revoked"
-                  class="fas fa-user-slash text-red-500"
+                  class="fas fa-user-slash text-red-500 mr-2"
                 />
-              </td>
-              <td class="pr-6 py-3 border-b">
-                <span class="font-medium">
-                  {{ user.username }}
-                </span>
-              </td>
-              <td class="px-6 py-3 border-b">
-                <a
-                  :href="'mailto:' + user.email"
-                  class="text-blue-600"
+                {{ user.username }}
+                <span
+                  v-if="user.revoked"
+                  class="text-xs mt-1 text-gray-600"
                 >
-                  {{ user.email }}
-                </a>
-              </td>
-              <td class="px-6 py-3 border-b">
-                <div class="text-xs">
-                  Succesvol: <span class="font-medium">{{ user.last_login }}</span>
+                  &bull; Revoked
+                </span>
+              </h3>
+              <p
+                v-show="user.remark"
+                class="mt-1 max-w-2xl text-sm text-gray-500"
+              >
+                {{ user.remark }}
+              </p>
+            </div>
+            <div class="bg-white border-gray-200">
+              <dl>
+                <div class="px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                  <dt class="text-sm font-medium text-gray-500">
+                    Email
+                  </dt>
+                  <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                    <ColorIcon
+                      class="bg-blue-500 text-white mr-2"
+                      icon="fa fa-envelope"
+                    />
+                    <a
+                      :href="'mailto:' + user.email"
+                      class="text-blue-500"
+                    >
+                      {{ user.email }}
+                    </a>
+                  </dd>
                 </div>
-                <div class="text-xs">
-                  Onsuccesvol: <span class="font-medium">{{ user.last_unsuccessful_login }}</span>
-                </div>
-              </td>
-            </tr>
-          </tbody>
-        </table>
+              </dl>
+              <div class="bg-white px-4 py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+                <dt class="text-sm font-medium text-gray-500">
+                  Login
+                </dt>
+                <dd class="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                  <template v-if="user.last_login">
+                    <ColorIcon
+                      class="bg-green-500 text-white mr-2"
+                      icon="fa fa-check"
+                    />
+                    <span class="font-medium">{{ user.last_login }}</span><br>
+                  </template>
+                  <template v-if="user.last_unsuccessful_login">
+                    <ColorIcon
+                      class="bg-red-500 text-white mr-2"
+                      icon="fa fa-times"
+                    />
+                    <span class="font-medium">{{ user.last_unsuccessful_login }}</span>
+                  </template>
+                </dd>
+              </div>
+            </div>
+            <div class="border-t px-4 py-5 sm:px-6 flex justify-between w-full">
+              <ButtonLink
+                class="bg-gray-900 text-white"
+                :route="{ name: 'admin.users' }"
+              >
+                <i class="fas fa-list mr-2" /> Detail
+              </ButtonLink>
+              <ButtonLink
+                v-if="canUpdate(user)"
+                class="bg-gray-900 text-white"
+                :route="{
+                  name: 'admin.users.edit',
+                  params: {
+                    uuid: user.uuid
+                  }
+                }"
+              >
+                <i class="fas fa-edit mr-2" /> Wijzig
+              </ButtonLink>
+            </div>
+          </div>
+        </div>
       </div>
     </PageSection>
   </div>
 </template>
 
-<script>
+<script setup>
 import PageSection from '/@theme/components/PageSection.vue';
 import PageSectionTitle from '/@theme/components/PageSectionTitle.vue';
+import { defineAbility } from '@casl/ability';
 import { computed } from 'vue';
 import { useUserStore } from '/src/apps/admin/stores/userStore.js';
 import StatCard from '/src/components/StatCard.vue';
+import { useAbility } from '/src/common/useAbility.js';
+import ColorIcon from '/src/components/ColorIcon.vue';
+import ButtonLink from '/src/components/ButtonLink.vue';
 
-export default {
-  components: { StatCard, PageSectionTitle, PageSection },
-  setup() {
-    const userStore = useUserStore();
-    const { loading } = userStore.load();
+const userStore = useUserStore();
+const { loading } = userStore.load();
 
-    const users = computed(() => userStore.users);
-    const count = computed(() => userStore.count);
+const users = computed(() => userStore.users);
+const count = computed(() => userStore.count);
 
-    return {
-      users,
-      count,
-      loading
-    };
-  }
-};
+const customAbility = defineAbility(can => {
+  can('update', 'user_accounts', { owner: true });
+});
+const ability = useAbility();
+ability.update(ability.rules.concat(customAbility.rules));
+
+const canUpdate = user => ability.can('update', user);
 </script>
