7bdc0f3190ed5f42dae98b23aa5b3a0a9f7c7d11
fbraem <franky.braem@gmail.com>
2022-03-19 21:17:26 +0100

    Add div wrapper

--- theme/kwai/components/PageSection.vue
+++ theme/kwai/components/PageSection.vue
@@ -1,6 +1,8 @@
 <template>
-  <section class="container mx-auto p-4 sm:py-8 lg:px-12">
-    <slot />
+  <section>
+    <div class="container mx-auto p-4 sm:py-8 lg:px-12">
+      <slot />
+    </div>
   </section>
 </template>
 
