575b57b3f1fcf5bcf529b1def50f8ebd6cf900f4
fbraem <franky.braem@gmail.com>
2022-08-03 22:03:36 +0200

    Solve the history.state problem

--- theme/kwai/components/SidebarNavigationItem.vue
+++ theme/kwai/components/SidebarNavigationItem.vue
@@ -1,29 +1,16 @@
 <template>
   <router-link
-    v-slot="{ href, navigate, isExactActive, route }"
-    custom
+    v-slot="{ route }"
     :to="{ name: routeName }"
+    exact-active-class="bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100 border-white"
+    class="flex items-center duration-200 mt-4 py-2 px-6 border-l-4 border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100"
+    @click="up()"
   >
-    <a
-      :href="href"
-      class="flex items-center duration-200 mt-4 py-2 px-6 border-l-4 hover:cursor-pointer"
-      :class="[isExactActive ? activeClass : inactiveClass]"
-      @click="go(navigate);"
-    >
-      {{ route.meta.title ?? 'meta.title missing' }}
-    </a>
+    {{ route.meta.title ?? 'meta.title missing' }}
   </router-link>
 </template>
 
 <script>
-import { ref } from 'vue';
-
-const scrollToTop = () => {
-  const top = document.querySelector('#main');
-  if (top) {
-    top.scrollTo(0, 0);
-  }
-};
 
 export default {
   props: {
@@ -33,22 +20,15 @@ export default {
     }
   },
   setup() {
-    const activeClass = ref(
-      'bg-gray-600 bg-opacity-25 text-gray-100 border-gray-100'
-    );
-    const inactiveClass = ref(
-      'border-gray-900 text-gray-500 hover:bg-gray-600 hover:bg-opacity-25 hover:text-gray-100'
-    );
-
-    const go = async(navigate) => {
-      await navigate();
-      scrollToTop();
+    const up = () => {
+      const top = document.querySelector('#main');
+      if (top) {
+        top.scrollTo(0, 0);
+      }
     };
 
     return {
-      activeClass,
-      inactiveClass,
-      go
+      up
     };
   }
 };
