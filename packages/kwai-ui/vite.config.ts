c58aa1fa868eca4e6f76c1395970f2a72a05a2e5
fbraem <franky.braem@gmail.com>
2022-08-22 21:20:41 +0200

    vite.config for kwai-ui

---
+++ packages/kwai-ui/vite.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import { resolve } from 'path'
+
+export default defineConfig({
+    plugins: [
+        vue(),
+        dts()
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, "src/index.ts"),
+            name: '@kwai/ui',
+            fileName: (format) => { console.log(format); return `index.${format}.js` }
+        },
+        rollupOptions: {
+            // make sure to externalize deps that shouldn't be bundled into the library
+            external: ['vue'],
+            output: {
+                // Provide global variables to use in the UMD build
+                // for externalized deps
+                globals: {
+                    vue: 'Vue'
+                }
+            }
+        }
+    },
+});
