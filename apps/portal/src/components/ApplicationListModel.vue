2f678a100e84f5c1892a9ab48bc9c0e8cc5c8f3d
fbraem <franky.braem@gmail.com>
2022-08-22 21:16:39 +0200

    A renderless component for looping applications

---
+++ apps/portal/src/components/ApplicationListModel.vue
@@ -0,0 +1,12 @@
+<template>
+  <slot :applications="applications" />
+</template>
+
+<script setup lang="ts">
+import { useApplicationStore } from "../stores/applicationStore"
+import { computed } from "vue"
+
+const store = useApplicationStore()
+store.load()
+const applications = computed(() => store.applications)
+</script>
