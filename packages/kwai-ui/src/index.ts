7c77a602f1a4fbaa460d0d3f58c02d50177b8cd0
fbraem <franky.braem@gmail.com>
2022-08-22 21:24:15 +0200

    Export all components

---
+++ packages/kwai-ui/src/index.ts
@@ -0,0 +1,15 @@
+import './index.css'
+import Card from './card/Card.vue'
+import CardTitle from './card/CardTitle.vue'
+import CardLinkedTitle from './card/CardLinkedTitle.vue'
+import CardRouterLinkedTitle from './card/CardRouterLinkedTitle.vue'
+
+export { Card, CardTitle, CardLinkedTitle, CardRouterLinkedTitle }
+
+import Toolbar from './nav/Toolbar.vue'
+import Sidebar from './nav/Sidebar.vue'
+import { useSidebar } from "./nav/useSidebar"
+export { Toolbar, Sidebar, useSidebar }
+
+import PortalLayout from './layout/PortalLayout.vue'
+export { PortalLayout }
