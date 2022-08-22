eeea2bd0ddc1123ac3873d740d0f3623c56f1d3a
fbraem <franky.braem@gmail.com>
2022-08-22 21:19:56 +0200

    Configure tailwindcss

---
+++ apps/portal/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        "./index.html",
+        "./src/**/*.{html,js,ts,vue,jsx,tsx}"
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [],
+}
