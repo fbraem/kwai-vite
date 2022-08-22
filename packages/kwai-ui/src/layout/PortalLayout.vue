87d044695088618b64a08dcab47a884e06972e62
fbraem <franky.braem@gmail.com>
2022-08-22 21:20:10 +0200

    Add PortalLayout

---
+++ packages/kwai-ui/src/layout/PortalLayout.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="flex h-screen">
+    <router-view name="sidebar" />
+    <div class="flex-1 flex flex-col overflow-hidden">
+      <router-view name="toolbar" />
+      <main
+        id="main"
+        class="flex-1 overflow-x-hidden overflow-y-auto"
+      >
+        <router-view name="main" />
+        <router-view name="footer" />
+      </main>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+</script>
