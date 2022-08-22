8961a6c198a99dbdcf11e863aa25d61fad2bd3a2
fbraem <franky.braem@gmail.com>
2022-08-22 20:59:23 +0200

    Add pina, routes

--- apps/portal/src/index.ts
+++ apps/portal/src/index.ts
@@ -1,5 +1,21 @@
 import { createApp } from 'vue'
 
 import App from './App.vue'
+import './index.css'
 
-createApp(App).mount("#app")
+import { createPinia } from 'pinia'
+import { createRouter, createWebHashHistory } from 'vue-router'
+
+const pinia = createPinia()
+const app = createApp(App)
+app.use(pinia)
+
+const router = createRouter({
+    history: createWebHashHistory(),
+    routes: []
+})
+import routes from "./routes"
+routes.forEach(route => router.addRoute(route))
+app.use(router)
+
+app.mount("#app")
