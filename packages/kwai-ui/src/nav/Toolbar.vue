af55abccd23d6b34bbbc9e221b49bc24e47468f7
fbraem <franky.braem@gmail.com>
2022-08-22 21:23:12 +0200

    Add nav components

---
+++ packages/kwai-ui/src/nav/Toolbar.vue
@@ -0,0 +1,32 @@
+<template>
+  <header class="flex justify-between items-center py-4 px-6">
+    <div class="flex items-center">
+      <button
+          class="focus:outline-none lg:hidden"
+          :class="fillColor"
+          @click="isOpen = true"
+      >
+        <BarsIcon class="w-4 h-4" />
+      </button>
+    </div>
+    <slot />
+  </header>
+</template>
+
+<script setup lang="ts">
+import { useSidebar } from "./useSidebar.js"
+import BarsIcon from "../icons/BarsIcon.vue"
+
+export interface Props {
+  fillColor: string
+}
+
+const props = withDefaults(
+    defineProps<Props>(),
+    {
+      fillColor: 'fill-black'
+    }
+)
+
+const { isOpen } = useSidebar()
+</script>
