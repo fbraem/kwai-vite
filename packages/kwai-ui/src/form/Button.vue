aa6be0b17d732cbce1404c850aa82bd0489e6e0f
fbraem <franky.braem@gmail.com>
2022-08-30 21:37:20 +0200

    Add some more padding and remove uppercase

--- packages/kwai-ui/src/form/Button.vue
+++ packages/kwai-ui/src/form/Button.vue
@@ -1,7 +1,6 @@
 <template>
   <a
-      v-bind="$attrs"
-      class="hover:cursor-pointer text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
+      class="hover:cursor-pointer text-sm font-bold px-12 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1"
       tabindex="0"
       @click="$emit('click')"
   >
