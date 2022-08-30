fce0c054b1c1d06249166c34335f94a7d26bdb9d
fbraem <franky.braem@gmail.com>
2022-08-30 21:39:43 +0200

    Reduce size on a xl screen and add padding

--- packages/kwai-ui/src/layout/DialogLayout.vue
+++ packages/kwai-ui/src/layout/DialogLayout.vue
@@ -5,13 +5,9 @@
         :style="styles"
     >
       <div class="w-full h-full absolute opacity-60 bg-black" />
-      <div class="container mx-auto px-4 h-full">
-        <div class="flex flex-col content-center items-center justify-center h-full">
-          <div class="w-full md:w-2/3 xl:w-1/2 px-4">
-            <div class="relative">
-              <router-view />
-            </div>
-          </div>
+      <div class="flex flex-col content-center items-center justify-center h-full">
+        <div class="w-full px-8 lg:w-2/3 xl:w-2/5 relative">
+          <router-view />
         </div>
       </div>
     </div>
