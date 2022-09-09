c61fd17d4964b5dc853f8aab800ee81e2ba8f207
fbraem <franky.braem@gmail.com>
2022-09-09 16:34:52 +0200

    Make it responsive, add error message, add httpLogin

--- apps/login/src/pages/LoginPage.vue
+++ apps/login/src/pages/LoginPage.vue
@@ -1,6 +1,23 @@
 <template>
-  <div class="grid grid-cols-5 break-words w-full mb-6 shadow-lg rounded-lg bg-white border-0">
-    <div class="flex flex-col col-span-3 pt-12 px-24">
+  <div class="flex flex-col sm:flex-row-reverse mb-6 shadow-lg rounded-lg bg-white max-w-6xl">
+    <div class="bg-red-700 w-full sm:w-1/2 md:w-1/3 sm:p-12 p-4 sm:rounded-r-lg">
+      <div class="flex flex-row sm:flex-col items-center">
+        <div class="w-16 mr-2">
+          <img src="logo.png" />
+        </div>
+        <h1 class="text-white text-2xl sm:text-center font-bold uppercase">
+          Judokwai Kemzeke
+        </h1>
+      </div>
+      <p class="text-gray-300 my-6">
+        A club account is available for members of the board and coaches.
+      </p>
+      <p class="text-gray-300">
+        Depending on the role in the club you will be able to manage several parts of the club.
+        Such as adding news, writing content, add trainings, etc.
+      </p>
+    </div>
+    <div class="flex flex-col w-full sm:w-1/2 md:w-2/3 pt-4 px-4 sm:px-12">
       <div class="mb-6">
         <div class="mb-3">
           <h6 class="text-gray-900 text-2xl font-bold">
@@ -36,6 +53,11 @@
             Forgot password?
           </a>
         </p>
+        <ErrorAlert v-if="errorMessage">
+          <div class="text-sm">
+            {{ errorMessage }}
+          </div>
+        </ErrorAlert>
         <div class="flex flex-col items-end mt-6">
             <Button
                 id="submit"
@@ -50,29 +72,15 @@
         &copy; Judokwai Kemzeke. All rights reserved
       </p>
     </div>
-    <div class="bg-red-700 col-span-2 rounded-r-lg p-12">
-      <div class="flex items-center">
-        <div class="w-32">
-          <img src="logo.png" />
-        </div>
-        <h1 class="text-white text-3xl font-bold text-center uppercase">
-          Judokwai Kemzeke
-        </h1>
-      </div>
-      <p class="text-gray-300 my-6">
-        A club account is available for members of the board and coaches.
-      </p>
-      <p class="text-gray-300">
-        Depending on the role in the club you will be able to manage several parts of the club.
-        Such as adding news, writing content, add trainings, etc.
-      </p>
-    </div>
   </div>
 </template>
 
 <script setup lang="ts">
-import { InputField, Button } from '@kwai/ui'
+import { InputField, Button, ErrorAlert } from '@kwai/ui'
 import { useForm } from "vee-validate"
+import { useHttpLogin } from "@kwai/api"
+import { ref } from "vue"
+import type { Ref } from "vue"
 
 function isRequired(value: string): string|boolean {
   if (value && value.trim()) {
@@ -96,7 +104,17 @@ const { handleSubmit } = useForm({
   }
 })
 
-const onSubmitForm = handleSubmit(values => {
-  console.log(values)
+const errorMessage: Ref<string|null> = ref(null)
+const onSubmitForm = handleSubmit(async values => {
+  errorMessage.value = null
+  const formData = {
+    username: values.email,
+    password: values.password
+  }
+  await useHttpLogin(formData).catch(error => {
+    if (error.response.status == 401) {
+      errorMessage.value = 'Login failed. Check your email and/or password.'
+    }
+  });
 })
 </script>
