ebd1a4c029389c213157777011feeaaea0fe7969
fbraem <franky.braem@gmail.com>
2022-10-22 19:33:33 +0200

    Config eslint

---
+++ packages/eslint-config-kwai/index.js
@@ -0,0 +1,12 @@
+module.exports = {
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@typescript-eslint/parser'
+  },
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: ['standard', 'plugin:vue/vue3-recommended'],
+  rules: {
+    semi: [2, 'always'],
+    'comma-dangle': ['error', 'never']
+  }
+};
