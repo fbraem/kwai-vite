569a478d0e6c60db90322f6dbab98dd7240cb9df
fbraem <franky.braem@gmail.com>
2022-10-23 18:43:17 +0200

    eslint / export icons

--- packages/kwai-ui/src/index.ts
+++ packages/kwai-ui/src/index.ts
@@ -1,28 +1,35 @@
-import './index.css'
+/* eslint-disable import/first */
+import './index.css';
 
-import Alert from './alerts/Alert.vue'
-import ErrorAlert from './alerts/ErrorAlert.vue'
-export { Alert, ErrorAlert }
+import Alert from './alerts/Alert.vue';
+import ErrorAlert from './alerts/ErrorAlert.vue';
+export { Alert, ErrorAlert };
 
-import Card from './card/Card.vue'
-import CardTitle from './card/CardTitle.vue'
-import CardLinkedTitle from './card/CardLinkedTitle.vue'
-import CardRouterLinkedTitle from './card/CardRouterLinkedTitle.vue'
+import Card from './card/Card.vue';
+import CardTitle from './card/CardTitle.vue';
+import CardLinkedTitle from './card/CardLinkedTitle.vue';
+import CardRouterLinkedTitle from './card/CardRouterLinkedTitle.vue';
 
-export { Card, CardTitle, CardLinkedTitle, CardRouterLinkedTitle }
+export { Card, CardTitle, CardLinkedTitle, CardRouterLinkedTitle };
 
-import InformationDialog from './dialogs/InformationDialog.vue'
-export { InformationDialog }
+import InformationDialog from './dialogs/InformationDialog.vue';
+export { InformationDialog };
 
-import Toolbar from './nav/Toolbar.vue'
-import Sidebar from './nav/Sidebar.vue'
-import { useSidebar } from "./nav/useSidebar"
-export { Toolbar, Sidebar, useSidebar }
+import Toolbar from './nav/Toolbar.vue';
+import Sidebar from './nav/Sidebar.vue';
+import { useSidebar } from './nav/useSidebar';
+export { Toolbar, Sidebar, useSidebar };
 
-import PortalLayout from './layout/PortalLayout.vue'
-import DialogLayout from './layout/DialogLayout.vue'
-export { PortalLayout, DialogLayout }
+import PortalLayout from './layout/PortalLayout.vue';
+import DialogLayout from './layout/DialogLayout.vue';
+export { PortalLayout, DialogLayout };
 
-import InputField from './form/InputField.vue'
-import Button from './form/Button.vue'
-export { InputField, Button }
+import InputField from './form/InputField.vue';
+import Button from './form/Button.vue';
+export { InputField, Button };
+
+import BarsIcon from './icons/BarsIcon.vue';
+import CheckIcon from './icons/CheckIcon.vue';
+import ErrorIcon from './icons/ErrorIcon.vue';
+import RequiredIcon from './icons/RequiredIcon.vue';
+export { BarsIcon, CheckIcon, ErrorIcon, RequiredIcon };
