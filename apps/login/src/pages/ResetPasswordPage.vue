f934908339048dc62c7aac8b0e8d431123714f89
fbraem <franky.braem@gmail.com>
2022-10-23 19:47:10 +0200

    Wrong import for useHttp

--- apps/login/src/pages/ResetPasswordPage.vue
+++ apps/login/src/pages/ResetPasswordPage.vue
@@ -121,12 +121,12 @@
 // eslint-disable-next-line import/no-absolute-path
 import logoUrl from '/logo.png';
 import config from '@kwai/config';
+import { useHttp } from '@kwai/api';
 import { InputField, Button, ErrorAlert, InformationDialog } from '@kwai/ui';
 import { useI18n } from 'vue-i18n';
 import { useTitle } from '@vueuse/core';
 import { useForm } from 'vee-validate';
 import { ref, Ref } from 'vue';
-import { useHttp } from '@root/packages/kwai-api';
 import { useRoute, useRouter } from 'vue-router';
 
 const { t } = useI18n({ useScope: 'global' });
