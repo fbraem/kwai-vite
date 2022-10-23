525fc965b50b73c4a6f12e80e62d4ebc7dd01f77
fbraem <franky.braem@gmail.com>
2022-10-23 19:41:05 +0200

    eslint

--- apps/login/vite.config.ts
+++ apps/login/vite.config.ts
@@ -1,48 +1,48 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import toml from '@fbraem/rollup-plugin-toml'
-import vueI18n from '@intlify/vite-plugin-vue-i18n'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import toml from '@fbraem/rollup-plugin-toml';
+import vueI18n from '@intlify/vite-plugin-vue-i18n';
 
-import { existsSync } from 'fs'
-import { resolve } from 'path'
+import { existsSync } from 'fs';
+import { resolve } from 'path';
 
 const resolveTheme = (path: string) => {
-    const themeFile = resolve(__dirname, `./src/theme/kwai${path}`)
-    // When the theme file exists, use it
-    if ( existsSync(themeFile)) {
-        console.log(`${path} resolved to the theme file ${themeFile}`)
-        return themeFile
-    }
-    // Otherwise use the default
-    const original = resolve(__dirname, `./src${path}`)
-    console.log(`No theme file found for ${path}, fallback on ${original}`);
-    return original
-}
+  const themeFile = resolve(__dirname, `./src/theme/kwai${path}`);
+  // When the theme file exists, use it
+  if (existsSync(themeFile)) {
+    console.log(`${path} resolved to the theme file ${themeFile}`);
+    return themeFile;
+  }
+  // Otherwise use the default
+  const original = resolve(__dirname, `./src${path}`);
+  console.log(`No theme file found for ${path}, fallback on ${original}`);
+  return original;
+};
 
 export default defineConfig({
-    base: '/login/',
-    server:  {
-        port: 3002,
-    },
-    plugins: [
-        vue(),
-        toml(),
-        vueI18n({
-            include: resolve(__dirname, './src/locales/**'),
-            compositionOnly: true
-        })
-    ],
-    resolve: {
-        alias: [
-            {
-                find: '@theme',
-                replacement: '',
-                customResolver: resolveTheme
-            },
-            {
-                find:  /^@root\/(.*)/,
-                replacement: `${resolve(__dirname, '../..')}/$1`
-            }
-        ]
-    }
-})
+  base: '/login/',
+  server: {
+    port: 3002
+  },
+  plugins: [
+    vue(),
+    toml(),
+    vueI18n({
+      include: resolve(__dirname, './src/locales/**'),
+      compositionOnly: true
+    })
+  ],
+  resolve: {
+    alias: [
+      {
+        find: '@theme',
+        replacement: '',
+        customResolver: resolveTheme
+      },
+      {
+        find: /^@root\/(.*)/,
+        replacement: `${resolve(__dirname)}/$1`
+      }
+    ]
+  }
+});
