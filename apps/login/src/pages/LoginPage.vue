89b3510c81eb5eba37c7d279bd64e9ab332b29f0
fbraem <franky.braem@gmail.com>
2022-10-23 17:59:31 +0200

    eslint

--- apps/login/src/pages/LoginPage.vue
+++ apps/login/src/pages/LoginPage.vue
@@ -3,7 +3,10 @@
     <template #information>
       <div class="flex flex-row sm:flex-col items-center">
         <div class="w-16 mr-2 sm:mb-4">
-          <img :src="logoUrl" alt="logo" />
+          <img
+            :src="logoUrl"
+            alt="logo"
+          >
         </div>
         <h1 class="text-white text-2xl sm:text-center font-bold uppercase">
           {{ config.website.title }}
@@ -22,26 +25,32 @@
           {{ t('login.title') }}
         </h6>
         <p class="text-sm text-gray-500">
-          {{ t('login.need_account') }} <a class="text-blue-400 font-medium" href="#">{{ t('login.contact_us') }}</a>
+          {{ t('login.need_account') }}
+          <a
+            class="text-blue-400 font-medium"
+            href="#"
+          >
+            {{ t('login.contact_us') }}
+          </a>
         </p>
       </div>
     </div>
     <form class="flex-auto">
       <InputField
-          name="email"
-          :placeholder="t('login.form.email.placeholder')"
-          class="mb-6"
-          :required="true"
+        name="email"
+        :placeholder="t('login.form.email.placeholder')"
+        class="mb-6"
+        :required="true"
       >
         <template #label>
           {{ t('login.form.email.label') }}
         </template>
       </InputField>
       <InputField
-          name="password"
-          type="password"
-          :placeholder="t('login.form.password.placeholder')"
-          :required="true"
+        name="password"
+        type="password"
+        :placeholder="t('login.form.password.placeholder')"
+        :required="true"
       >
         <template #label>
           {{ t('login.form.password.label') }}
@@ -49,8 +58,8 @@
       </InputField>
       <p class="text-right text-sm mt-1">
         <router-link
-            class="text-blue-400"
-            to="recover"
+          class="text-blue-400"
+          to="recover"
         >
           {{ t('login.forgotten') }}
         </router-link>
@@ -61,13 +70,13 @@
         </div>
       </ErrorAlert>
       <div class="flex flex-col items-end mt-6">
-          <Button
-              id="submit"
-              class="bg-gray-700 text-white focus:bg-gray-900 z-20"
-              @click="onSubmitForm"
-          >
-            {{ t('login.form.submit.label') }}
-          </Button>
+        <Button
+          id="submit"
+          class="bg-gray-700 text-white focus:bg-gray-900 z-20"
+          @click="onSubmitForm"
+        >
+          {{ t('login.form.submit.label') }}
+        </Button>
       </div>
     </form>
     <p class="mt-12 mb-3 text-center text-xs text-gray-500">
@@ -77,51 +86,52 @@
 </template>
 
 <script setup lang="ts">
-import logoUrl from '/logo.png'
+// eslint-disable-next-line import/no-absolute-path
+import logoUrl from '/logo.png';
 
-import config from "@kwai/config"
-import { InputField, Button, ErrorAlert, InformationDialog } from "@kwai/ui"
-import { useForm } from "vee-validate"
-import { useHttpLogin } from "@kwai/api"
-import { ref } from "vue"
-import type { Ref } from "vue"
-import { useI18n } from "vue-i18n"
+import config from '@kwai/config';
+import { InputField, Button, ErrorAlert, InformationDialog } from '@kwai/ui';
+import { useForm } from 'vee-validate';
+import { useHttpLogin } from '@kwai/api';
+import { ref } from 'vue';
+import type { Ref } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-const { t } = useI18n({ useScope: 'global' })
+const { t } = useI18n({ useScope: 'global' });
 
 function isRequired(value: string): string|boolean {
   if (value && value.trim()) {
-    return true
+    return true;
   }
-  return t('login.required')
+  return t('login.required');
 }
 
 function isEmail(value: string): string|boolean {
   const regex = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i;
   if (!regex.test(value)) {
-    return t('login.invalid_email')
+    return t('login.invalid_email');
   }
-  return true
+  return true;
 }
 
 const { handleSubmit } = useForm({
   validationSchema: {
-    email: [ isRequired, isEmail ],
+    email: [isRequired, isEmail],
     password: isRequired
   }
-})
+});
 
-const errorMessage: Ref<string|null> = ref(null)
+const errorMessage: Ref<string|null> = ref(null);
 const onSubmitForm = handleSubmit(async values => {
-  errorMessage.value = null
+  errorMessage.value = null;
   const formData = {
     username: values.email,
     password: values.password
-  }
+  };
   await useHttpLogin(formData).catch(error => {
-    if (error.response.status == 401) {
-      errorMessage.value = t('login.failed')
+    if (error.response.status === 401) {
+      errorMessage.value = t('login.failed');
     }
   });
-})
+});
 </script>
