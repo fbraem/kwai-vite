a731e0d14eb77fb09bc992822ebced0c8a9c3ed8
fbraem <franky.braem@gmail.com>
2022-10-25 20:50:09 +0200

    esling / Add api

--- packages/kwai-config/src/config.d.ts
+++ packages/kwai-config/src/config.d.ts
@@ -1,7 +1,11 @@
+/* eslint-disable no-unused-vars */
 declare module '@kwai/config' {
     const website: {
         title: string
         copyright: string
         email: string
-    }
+    };
+    const api: {
+        base_url: string
+    };
 }
