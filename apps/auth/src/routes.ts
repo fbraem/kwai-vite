dbfdbb3be97b910fe8ad0b11c70e52b0392cbe7b
fbraem <franky.braem@gmail.com>
2022-10-25 21:36:23 +0200

    eslint

--- apps/auth/src/routes.ts
+++ apps/auth/src/routes.ts
@@ -1,40 +1,41 @@
-import { DialogLayout } from "@kwai/ui"
-import { RouteRecordRaw } from "vue-router"
-import LoginPage from "./pages/LoginPage.vue"
-import InvitedPage from "./pages/InvitedPage.vue"
-import RecoverPasswordPage from "./pages/RecoverPasswordPage.vue"
-import ResetPasswordPage from "./pages/ResetPasswordPage.vue"
+import { DialogLayout } from '@kwai/ui';
+import { RouteRecordRaw } from 'vue-router';
+import LoginPage from './pages/LoginPage.vue';
+import InvitedPage from './pages/InvitedPage.vue';
+import RecoverPasswordPage from './pages/RecoverPasswordPage.vue';
+import ResetPasswordPage from './pages/ResetPasswordPage.vue';
 
-import heroImageUrl from '/hero.jpg'
+// eslint-disable-next-line import/no-absolute-path
+import heroImageUrl from '/hero.jpg';
 
 export const routes: RouteRecordRaw[] = [
-    {
+  {
+    path: '/',
+    component: DialogLayout,
+    props: {
+      image: heroImageUrl
+    },
+    children: [
+      {
+        name: 'login',
         path: '/',
-        component: DialogLayout,
-        props: {
-            image: heroImageUrl
-        },
-        children: [
-            {
-                name: 'login',
-                path: '/',
-                component: LoginPage
-            },
-            {
-                name: 'invited',
-                path: '/invited',
-                component: InvitedPage
-            },
-            {
-                name: 'recover',
-                path: '/recover',
-                component: RecoverPasswordPage
-            },
-            {
-                name: 'reset',
-                path: '/reset',
-                component: ResetPasswordPage
-            }
-        ]
-    }
-]
+        component: LoginPage
+      },
+      {
+        name: 'invited',
+        path: '/invited',
+        component: InvitedPage
+      },
+      {
+        name: 'recover',
+        path: '/recover',
+        component: RecoverPasswordPage
+      },
+      {
+        name: 'reset',
+        path: '/reset',
+        component: ResetPasswordPage
+      }
+    ]
+  }
+];
