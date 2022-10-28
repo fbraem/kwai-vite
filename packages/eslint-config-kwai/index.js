43968a742e25c34adea2426b03f8f074ec256439
fbraem <franky.braem@gmail.com>
2022-10-28 19:30:59 +0200

    Dangling comma required on multiline

--- packages/eslint-config-kwai/index.js
+++ packages/eslint-config-kwai/index.js
@@ -1,13 +1,13 @@
 module.exports = {
   parser: 'vue-eslint-parser',
   parserOptions: {
-    parser: '@typescript-eslint/parser'
+    parser: '@typescript-eslint/parser',
   },
   plugins: ['@typescript-eslint', 'prettier'],
   extends: ['standard', 'plugin:vue/vue3-recommended'],
   rules: {
     semi: [2, 'always'],
-    'comma-dangle': ['error', 'never'],
-    'space-before-function-paren': [2, 'never']
-  }
+    'comma-dangle': ['error', 'always-multiline'],
+    'space-before-function-paren': [2, 'never'],
+  },
 };
