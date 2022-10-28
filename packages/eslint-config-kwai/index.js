6517b2afdc7ed0b9109156214208336785272f23
fbraem <franky.braem@gmail.com>
2022-10-28 19:36:01 +0200

    No dangling comma for function arguments

--- packages/eslint-config-kwai/index.js
+++ packages/eslint-config-kwai/index.js
@@ -7,7 +7,13 @@ module.exports = {
   extends: ['standard', 'plugin:vue/vue3-recommended'],
   rules: {
     semi: [2, 'always'],
-    'comma-dangle': ['error', 'always-multiline'],
+    'comma-dangle': ['error', {
+      arrays: 'always-multiline',
+      objects: 'always-multiline',
+      imports: 'always-multiline',
+      exports: 'always-multiline',
+      functions: 'never',
+    }],
     'space-before-function-paren': [2, 'never'],
   },
 };
