41e39098f9ae44f59101cef0c4b12ec304fb7243
fbraem <franky.braem@gmail.com>
2022-11-01 13:35:39 +0100

    eslint

---
+++ apps/portal/src/pages/HomePage.vue
@@ -0,0 +1,58 @@
+<template>
+  <div>
+    <div class="container mx-auto p-4">
+      <div class="grid gap-8 sm:grid-cols-1 sm:grid-rows-3 md:grid-cols-3 md:grid-rows-1 justify-items-center">
+        <ApplicationList>
+          <template
+            v-for="cardName in applicationCards"
+            :key="cardName"
+            #[`app-${cardName}`]="{ application }"
+          >
+            <Card class="max-w-sm p-6 w-full">
+              <template #header>
+                <CardRouterLinkedTitle
+                  :to="{ name: 'home' }"
+                  class="text-2xl font-bold tracking-tight text-gray-900 dark:text-white inline-flex items-center"
+                >
+                  <div
+                    v-if="listedApplications[application.name]"
+                    class="rounded-full w-8 h-8 bg-red-600 inline-flex items-center justify-center mr-1"
+                  >
+                    <component
+                      :is="listedApplications[application.name]"
+                      class="w-4 h-4 fill-white"
+                    />
+                  </div>
+                  {{ application.title }}
+                </CardRouterLinkedTitle>
+              </template>
+              <p class="mb-3 font-normal text-gray-500 dark:text-gray-400">
+                {{ application.short_description }}
+              </p>
+            </Card>
+          </template>
+        </ApplicationList>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import ApplicationList from '../components/ApplicationList.vue';
+import { Card, CardRouterLinkedTitle } from '@kwai/ui';
+import { ref, ShallowReactive, shallowReactive } from 'vue';
+import ClubIcon from '@theme/icons/ClubIcon.vue';
+import NewsIcon from '@theme/icons/NewsIcon.vue';
+import TrainingIcon from '@theme/icons/TrainingIcon.vue';
+
+interface ListedApplications {
+  [key: string]: ShallowReactive<any>
+}
+
+const listedApplications: ListedApplications = shallowReactive({
+  club: ClubIcon,
+  news: NewsIcon,
+  trainings: TrainingIcon,
+});
+const applicationCards = ref(['club', 'news', 'trainings']);
+</script>
--- apps/portal/src/routes.ts
+++ apps/portal/src/routes.ts
@@ -1,22 +1,22 @@
-import { RouteRecordRaw } from "vue-router"
-import HomePage from "@theme/pages/Home.vue"
-import PortalToolbar from "./components/PortalToolbar.vue"
-import { PortalLayout } from "@kwai/ui"
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '@theme/pages/HomePage.vue';
+import PortalToolbar from './components/PortalToolbar.vue';
+import { PortalLayout } from '@kwai/ui';
 
 const routes: RouteRecordRaw[] = [
-    {
+  {
+    path: '/',
+    component: PortalLayout,
+    children: [
+      {
+        name: 'home',
         path: '/',
-        component: PortalLayout,
-        children: [
-            {
-                name: 'home',
-                path: '/',
-                components: {
-                    toolbar: PortalToolbar,
-                    main: HomePage
-                }
-            }
-        ]
-    }
-]
-export default routes
+        components: {
+          toolbar: PortalToolbar,
+          main: HomePage,
+        },
+      },
+    ],
+  },
+];
+export default routes;
