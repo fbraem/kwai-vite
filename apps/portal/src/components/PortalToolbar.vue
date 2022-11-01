9c462e6819cfdb48a3586d2e1f3c0edc4ccd834b
fbraem <franky.braem@gmail.com>
2022-11-01 13:38:21 +0100

    eslint

--- apps/portal/src/components/PortalToolbar.vue
+++ apps/portal/src/components/PortalToolbar.vue
@@ -1,10 +1,10 @@
 <template>
   <Toolbar
-      class="bg-red-600"
-      fill-color="fill-white"
+    class="bg-red-600"
+    fill-color="fill-white"
   />
 </template>
 
 <script setup lang="ts">
-import { Toolbar } from "@kwai/ui"
+import { Toolbar } from '@kwai/ui';
 </script>
