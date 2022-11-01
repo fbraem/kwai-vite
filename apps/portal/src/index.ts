ee14bcb238cbcbb6e7e1fbb17a2f73b48faf86e1
fbraem <franky.braem@gmail.com>
2022-11-01 19:23:30 +0100

    eslint

--- apps/portal/src/index.ts
+++ apps/portal/src/index.ts
@@ -1,22 +1,22 @@
-import { createApp } from 'vue'
+import { createApp } from 'vue';
 
-import App from './App.vue'
-import './index.css'
-import "@kwai/ui/style.css"
+import App from './App.vue';
+import './index.css';
+import '@kwai/ui/style.css';
 
-import { createPinia } from 'pinia'
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createPinia } from 'pinia';
+import { createRouter, createWebHashHistory } from 'vue-router';
+import routes from './routes';
 
-const pinia = createPinia()
-const app = createApp(App)
-app.use(pinia)
+const pinia = createPinia();
+const app = createApp(App);
+app.use(pinia);
 
 const router = createRouter({
-    history: createWebHashHistory(),
-    routes: []
-})
-import routes from "./routes"
-routes.forEach(route => router.addRoute(route))
-app.use(router)
+  history: createWebHashHistory(),
+  routes: [],
+});
+routes.forEach(route => router.addRoute(route));
+app.use(router);
 
-app.mount("#app")
+app.mount('#app');
