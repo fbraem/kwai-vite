0bb46d2d955a8b0394f9742c0edc6f51990b1239
fbraem <franky.braem@gmail.com>
2022-11-01 18:08:54 +0100

    Add package kwai-ui

--- apps/portal/tailwind.config.js
+++ apps/portal/tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
   content: [
     './index.html',
     './src/**/*.{html,js,ts,vue,jsx,tsx}',
+    '../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}',
   ],
   theme: {
     extend: {},
