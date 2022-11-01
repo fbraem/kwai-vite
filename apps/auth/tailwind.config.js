0b98409233f75110b3361c2b91ca783df15dfb90
fbraem <franky.braem@gmail.com>
2022-11-01 18:07:44 +0100

    eslint

--- apps/auth/tailwind.config.js
+++ apps/auth/tailwind.config.js
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        "./index.html",
-        "./src/**/*.{html,js,ts,vue,jsx,tsx}",
-        "../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}"
-    ],
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
+  content: [
+    './index.html',
+    './src/**/*.{html,js,ts,vue,jsx,tsx}',
+    '../../packages/kwai-ui/src/**/*.{html,js,ts,vue,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
