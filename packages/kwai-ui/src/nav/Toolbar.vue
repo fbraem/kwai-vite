20371767ee509be2d9daf0b4cb76db527115178b
fbraem <franky.braem@gmail.com>
2022-11-01 13:43:44 +0100

    Replace header with div

--- packages/kwai-ui/src/nav/Toolbar.vue
+++ packages/kwai-ui/src/nav/Toolbar.vue
@@ -1,10 +1,10 @@
 <template>
-  <header class="flex justify-between items-center py-4 px-6">
+  <div class="flex justify-between items-center py-4 px-6">
     <div class="flex items-center">
       <button
-          class="focus:outline-none lg:hidden"
-          :class="fillColor"
-          @click="isOpen = true"
+        class="focus:outline-none lg:hidden"
+        :class="fillColor"
+        @click="isOpen = true"
       >
         <BarsIcon class="w-4 h-4" />
       </button>
@@ -16,23 +16,23 @@
     <div>
       <slot name="right" />
     </div>
-  </header>
+  </div>
 </template>
 
 <script setup lang="ts">
-import { useSidebar } from "./useSidebar.js"
-import BarsIcon from "../icons/BarsIcon.vue"
+import { useSidebar } from './useSidebar.js';
+import BarsIcon from '../icons/BarsIcon.vue';
 
-export interface Props {
+interface Props {
   fillColor: string
 }
 
-const props = withDefaults(
-    defineProps<Props>(),
-    {
-      fillColor: 'fill-black'
-    }
-)
+withDefaults(
+  defineProps<Props>(),
+  {
+    fillColor: 'fill-black',
+  }
+);
 
-const { isOpen } = useSidebar()
+const { isOpen } = useSidebar();
 </script>
