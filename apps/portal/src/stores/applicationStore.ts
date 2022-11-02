155aa79404a768ca619f35cbec54229376fcb072
fbraem <franky.braem@gmail.com>
2022-11-02 19:30:47 +0100

    eslint

--- apps/portal/src/stores/applicationStore.ts
+++ apps/portal/src/stores/applicationStore.ts
@@ -1,8 +1,8 @@
-import { defineStore } from "pinia"
-import { ref, watch } from "vue"
-import type { Ref } from "vue"
-import { useHttp } from "@kwai/api"
-import { useRequest } from "vue-request"
+import { defineStore } from 'pinia';
+import { ref, watch } from 'vue';
+import type { Ref } from 'vue';
+import { useHttp } from '@kwai/api';
+import { useRequest } from 'vue-request';
 
 interface JSONDataType<DataType> {
     id: string,
@@ -32,49 +32,49 @@ interface Application {
 }
 
 export const useApplicationStore = defineStore(
-    'applications',
-    () => {
-        const application: Ref<Application|null> = ref(null)
-        const applications: Ref<Application[]> = ref([])
+  'applications',
+  () => {
+    const application: Ref<Application|null> = ref(null);
+    const applications: Ref<Application[]> = ref([]);
 
-        const toModel = (json: JSONAPI<JSONAPIApplication>): Application | Application[] => {
-            const mapModel = (d: JSONDataType<JSONAPIApplication>): Application => ({
-                id: d.id,
-                title: d.attributes.title,
-                name: d.attributes.name,
-                short_description: d.attributes.short_description
-            })
-            if (Array.isArray(json.data)) {
-                return json.data.map(mapModel)
-            }
-            return mapModel(json.data)
-        }
+    const toModel = (json: JSONAPI<JSONAPIApplication>): Application | Application[] => {
+      const mapModel = (d: JSONDataType<JSONAPIApplication>): Application => ({
+        id: d.id,
+        title: d.attributes.title,
+        name: d.attributes.name,
+        short_description: d.attributes.short_description,
+      });
+      if (Array.isArray(json.data)) {
+        return json.data.map(mapModel);
+      }
+      return mapModel(json.data);
+    };
 
-        const load = () => {
-            const { data, loading, error } = useRequest(
-                () => {
-                    let api = useHttp()
-                        .url('/portal/applications')
-                    return api.get().json()
-                },
-                {
-                    cacheKey: '/portal/applications',
-                    errorRetryCount: 5,
-                    refreshOnWindowFocus: false
-                }
-            )
-            watch(
-                data,
-                json => {
-                    applications.value = toModel(json as JSONAPI<JSONAPIApplication>) as Application[]
-                }
-            )
+    const load = () => {
+      const { data, loading, error } = useRequest(
+        () => {
+          const api = useHttp()
+            .url('/portal/applications');
+          return api.get().json();
+        },
+        {
+          cacheKey: '/portal/applications',
+          errorRetryCount: 5,
+          refreshOnWindowFocus: false,
         }
+      );
+      watch(
+        data,
+        json => {
+          applications.value = toModel(json as JSONAPI<JSONAPIApplication>) as Application[];
+        }
+      );
+    };
 
-        return {
-            application,
-            applications,
-            load
-        }
-    }
-)
+    return {
+      application,
+      applications,
+      load,
+    };
+  }
+);
