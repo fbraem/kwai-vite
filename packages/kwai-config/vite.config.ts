a8789bc45ea505f9367803556c96292b88f181c7
fbraem <franky.braem@gmail.com>
2022-11-02 14:33:16 +0100

    eslint

--- packages/kwai-config/vite.config.ts
+++ packages/kwai-config/vite.config.ts
@@ -1,21 +1,21 @@
-import { defineConfig } from "vite"
-import toml from "@fbraem/rollup-plugin-toml"
-import { resolve } from "path"
+import { defineConfig } from 'vite';
+import toml from '@fbraem/rollup-plugin-toml';
+import { resolve } from 'path';
 
 export default defineConfig({
-    plugins: [
-        toml()
-    ],
-    build: {
-        lib: {
-            entry: resolve(__dirname, "src/config.toml"),
-            name: "@kwai/config",
-            fileName: (format) => { console.log(format); return `index.${format}.js` }
-        },
-        rollupOptions: {
-            output: {
-                exports: "named"
-            }
-        }
+  plugins: [
+    toml(),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/config.toml'),
+      name: '@kwai/config',
+      fileName: (format) => { console.log(format); return `index.${format}.js`; },
     },
+    rollupOptions: {
+      output: {
+        exports: 'named',
+      },
+    },
+  },
 });
