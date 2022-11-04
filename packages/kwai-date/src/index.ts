e467d14955cbbcf73698526efe97bb5ab3f3655f
fbraem <franky.braem@gmail.com>
2022-11-04 22:01:09 +0100

    Add createDateTimeFromUTC, import with .js, export DateType, add wrapDayjs

--- packages/kwai-date/src/index.ts
+++ packages/kwai-date/src/index.ts
@@ -1,16 +1,16 @@
 import dayjs from 'dayjs';
 
 // Load all necessary extensions
-import utc from 'dayjs/plugin/utc';
-import timezone from 'dayjs/plugin/timezone';
-import localizedFormat from 'dayjs/plugin/localizedFormat';
-import localeData from 'dayjs/plugin/localeData';
-import weekday from 'dayjs/plugin/weekday';
-import customParseFormat from 'dayjs/plugin/customParseFormat';
+import utc from 'dayjs/plugin/utc.js';
+import timezone from 'dayjs/plugin/timezone.js';
+import localizedFormat from 'dayjs/plugin/localizedFormat.js';
+import localeData from 'dayjs/plugin/localeData.js';
+import weekday from 'dayjs/plugin/weekday.js';
+import customParseFormat from 'dayjs/plugin/customParseFormat.js';
 
 // Set the locale
-import 'dayjs/locale/en';
-import 'dayjs/locale/nl';
+import 'dayjs/locale/en.js';
+import 'dayjs/locale/nl.js';
 dayjs.extend(utc);
 dayjs.extend(timezone);
 dayjs.extend(localizedFormat);
@@ -20,13 +20,11 @@ dayjs.extend(customParseFormat);
 // TODO: For now, use the nl locale, in the future this must be configurable.
 dayjs.locale('nl');
 
-interface DateType {
+export interface DateType {
     format(format: string): string
 }
 
-export function createDate(value?: string, fmt: string = 'YYYY-MM-DD'): Readonly<DateType> {
-  const d = value ? dayjs(value, fmt) : dayjs();
-
+function wrapDayjs(d: dayjs.Dayjs): Readonly<DateType> {
   function format(format: string = 'YYYY-MM-DD'): string {
     return d.format(format);
   }
@@ -36,8 +34,16 @@ export function createDate(value?: string, fmt: string = 'YYYY-MM-DD'): Readonly
   });
 }
 
+export function createDate(value?: string, fmt: string = 'YYYY-MM-DD'): Readonly<DateType> {
+  return wrapDayjs(value ? dayjs(value, fmt) : dayjs());
+}
+
 export function createDatetime(value?: string, fmt: string = 'YYYY-MM-DD HH:mm:ss'): Readonly<DateType> {
   return createDate(value, fmt);
+}
+
+export function createDateTimeFromUTC(value: string, timezone: string, fmt: string = 'YYYY-MM-DD HH:mm:ss'): Readonly<DateType> {
+  return wrapDayjs(dayjs.utc(value, fmt).tz(timezone));
 }
 
 export function now(): Readonly<DateType> {
