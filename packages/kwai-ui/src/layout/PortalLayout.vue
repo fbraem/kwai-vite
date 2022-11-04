51192c7634bf6aa3b3def6660779259e0fb0bddd
fbraem <franky.braem@gmail.com>
2022-11-04 22:51:31 +0100

    Remove overflow

--- packages/kwai-ui/src/layout/PortalLayout.vue
+++ packages/kwai-ui/src/layout/PortalLayout.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="flex h-screen">
     <router-view name="sidebar" />
-    <div class="flex-1 flex flex-col overflow-hidden">
+    <div class="flex-1 flex flex-col">
       <router-view name="toolbar" />
       <main
         id="main"
-        class="flex-1 overflow-x-hidden overflow-y-auto"
+        class="flex-1"
       >
         <router-view name="main" />
         <router-view name="footer" />
