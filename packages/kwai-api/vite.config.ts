fec5887d61d88e3e82693ef36bbf3cf81aadb004
fbraem <franky.braem@gmail.com>
2022-11-12 21:17:10 +0100

    Use vite-plugin-dts

--- packages/kwai-api/vite.config.ts
+++ packages/kwai-api/vite.config.ts
@@ -1,14 +1,16 @@
 import { defineConfig } from 'vite';
 import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 export default defineConfig({
   plugins: [
+    dts(),
   ],
   build: {
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: '@kwai/api',
-      fileName: (format) => { return `index.${format}.js`; },
+      fileName: 'kwai-api',
     },
   },
 });
