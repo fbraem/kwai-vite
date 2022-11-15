a08791068b586d6afe4e746285687826a64fcdf7
fbraem <franky.braem@gmail.com>
2022-11-15 21:58:42 +0100

    Use zod for jsonapi and enhance load

--- apps/portal/src/stores/trainingStore.ts
+++ apps/portal/src/stores/trainingStore.ts
@@ -1,15 +1,15 @@
 import { defineStore } from 'pinia';
 import { Ref, ref } from 'vue';
-import { createDateTimeFromUTC, DateType } from '@kwai/date';
+import { createDateTimeFromUTC, DateType, now } from '@kwai/date';
 import { useRequest } from 'vue-request';
 import { z } from 'zod';
-import { useHttpApi, JsonApiDataType, JsonApiDocumentType } from '@kwai/api';
+import { useHttpApi, JsonApiDataType, JsonApiDocument } from '@kwai/api';
 
 const JsonApiEvent = z.object({
   start_date: z.string(),
   end_date: z.string(),
   timezone: z.string(),
-  location: z.string().optional(),
+  location: z.nullable(z.string()).optional(),
   cancelled: z.boolean(),
   active: z.boolean(),
 });
@@ -18,7 +18,7 @@ const JsonApiContent = z.object({
   locale: z.string(),
   title: z.string(),
   html_summary: z.string(),
-  html_content: z.string().optional(),
+  html_content: z.nullable(z.string()).optional(),
 });
 
 const JsonApiTeam = z.object({
@@ -30,6 +30,22 @@ const JsonApiTeam = z.object({
 });
 type JsonApiTeamType = z.infer<typeof JsonApiTeam>;
 
+const JsonApiCoach = z.object({
+  id: z.string(),
+  type: z.literal('coaches'),
+  attributes: z.object({
+    name: z.string(),
+  }),
+});
+
+const JsonApiDefinition = z.object({
+  id: z.string(),
+  type: z.literal('definitions'),
+  attributes: z.object({
+    name: z.string(),
+  }),
+});
+
 const JsonApiTraining = z.object({
   id: z.string(),
   type: z.literal('trainings'),
@@ -39,6 +55,14 @@ const JsonApiTraining = z.object({
   }),
 });
 type JsonApiTrainingType = z.infer<typeof JsonApiTraining>;
+
+const JsonApiTrainingData = z.object({
+  data: z.union([JsonApiTraining, z.array(JsonApiTraining).default([])]),
+  included: z.array(z.union([JsonApiTeam, JsonApiCoach, JsonApiDefinition])).default([]),
+});
+
+const JsonApiTrainingDocument = JsonApiDocument.extend(JsonApiTrainingData.shape);
+type JsonApiTrainingDocumentType = z.infer<typeof JsonApiTrainingDocument>;
 
 type Team = {
   id: string,
@@ -50,28 +74,26 @@ type Training = {
   cancelled: boolean,
   start_date: DateType,
   end_date: DateType,
-  location?: string,
+  location?: string | null,
   title: string,
   summary: string,
-  content?: string,
+  content?: string | null,
   teams: Team[],
 };
 
-const toModel = (json: JsonApiDocumentType): Training | Training[] => {
+const toModel = (json: JsonApiTrainingDocumentType): Training | Training[] => {
   const mapModel = (d: JsonApiDataType): Training => {
     const training = <JsonApiTrainingType> d;
     const teams: JsonApiTeamType[] = [];
-    if (d.relationships?.teams) {
-      if (Array.isArray(d.relationships.teams.data)) {
-        d.relationships.teams.data.forEach(t => {
-          const includedTeam = json.included?.find(
-            included => included.type === 'teams' && included.id === t.id
-          );
-          if (includedTeam) {
-            teams.push(includedTeam as JsonApiTeamType);
-          }
-        });
-      }
+    if (d.relationships?.teams && Array.isArray(d.relationships.teams.data)) {
+      d.relationships.teams.data.forEach(t => {
+        const includedTeam = json.included?.find(
+          included => included.type === 'teams' && included.id === t.id
+        );
+        if (includedTeam) {
+          teams.push(includedTeam as JsonApiTeamType);
+        }
+      });
     }
     return {
       id: training.id,
@@ -97,21 +119,44 @@ const toModel = (json: JsonApiDocumentType): Training | Training[] => {
 export const useTrainingStore = defineStore('portal.trainings', () => {
   const trainings: Ref<Training[]> = ref([]);
 
-  const load = () => {
+  const start = ref(now());
+  const end = ref(now().endOf('week'));
+
+  const load = ({
+    offset = ref(0),
+    limit = ref(0),
+  } = {}) => {
     const { loading, error } = useRequest(
       () => {
-        const api = useHttpApi().url('/trainings');
+        let api = useHttpApi().url('/trainings');
+        if (offset.value > 0) {
+          api = api.query({ 'page[offset]': offset.value });
+        }
+        if (limit.value) {
+          api = api.query({ 'page[limit]': limit.value });
+        }
+        api = api.query({
+          'filter[start]': start.value.format(),
+          'filter[end]': end.value.format(),
+        });
         return api
           .get()
           .json()
         ;
       },
       {
-        cacheKey: 'portal.trainings',
+        refreshDeps: [offset, limit, start, end],
+        cacheKey: `portal.trainings/${offset.value}/${limit.value}`,
         errorRetryCount: 5,
         refreshOnWindowFocus: false,
         onSuccess: data => {
-          trainings.value = <Training[]> toModel(<JsonApiDocumentType> data);
+          const result = JsonApiTrainingDocument.safeParse(data);
+          if (result.success) {
+            trainings.value = <Training[]>toModel(result.data);
+          } else {
+            console.log(result.error);
+            trainings.value = [];
+          }
         },
       }
     );
