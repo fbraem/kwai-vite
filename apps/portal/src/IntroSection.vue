fa9edb115980f1fb1afd3a6c125b8bd2615840a1
fbraem <franky.braem@gmail.com>
2022-11-19 16:48:26 +0100

    Add IntroSection

---
+++ apps/portal/src/IntroSection.vue
@@ -0,0 +1,24 @@
+<template>
+  <section>
+    <div
+      class="relative"
+      style="height:600px;"
+    >
+      <div
+        class="absolute w-full h-full bg-center xl:bg-top bg-cover"
+        :style="{ 'background-image': 'url(' + heroImageUrl + ')' }"
+      >
+        <span class="w-full h-full absolute opacity-50 bg-gradient-to-br from-black to-red-600" />
+        <slot />
+      </div>
+    </div>
+  </section>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  heroImageUrl: string
+}
+
+defineProps<Props>();
+</script>
