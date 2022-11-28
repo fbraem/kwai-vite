ad40665b7d7fb84dee5a5ac8e0a7f1a6f070ea82
fbraem <franky.braem@gmail.com>
2022-11-28 20:43:07 +0100

    Introduce NewsCard

--- apps/portal/src/components/home/HomeSectionNews.vue
+++ apps/portal/src/components/home/HomeSectionNews.vue
@@ -9,48 +9,10 @@
           Het belangrijkste <strong>nieuws</strong> van onze club op een rijtje...
         </p>
       </div>
-      <div class="grid md:grid-cols-2 lg:grid-cols-3">
+      <div class="grid md:grid-cols-2 lg:grid-cols-3 gap-4">
         <NewsList :promoted="true">
           <template #default="{ story }">
-            <div class="mb-7 px-4 flex flex-col">
-              <router-link
-                class="block flex items-end bg-cover"
-                style="height: 200px;"
-                :style="{ 'background-image': `url(${newsImage})` }"
-                to="/news"
-              >
-                <div class="text-center p-2 bg-red-600 text-white min-w-fit">
-                  <span class="block font-bold text-xl">
-                    {{ story.publish_date.format('D') }}
-                  </span>
-                  <span class="block text-sm">
-                    {{ story.publish_date.format('MMM') }}
-                  </span>
-                  <span class="block text-sm">
-                    {{ story.publish_date.format('YYYY') }}
-                  </span>
-                </div>
-              </router-link>
-              <div class="p-6 bg-white flex-1 grid auto-rows-min">
-                <h3 class="text-xl mb-4">
-                  <router-link to="/news">
-                    {{ story.contents[0].title }}
-                  </router-link>
-                </h3>
-                <p
-                  class="text-gray-500 flex-grow"
-                  v-html="story.contents[0].summary"
-                />
-                <div class="mt-6">
-                  <router-link
-                    class="border border-red-600 bg-red-600 hover:bg-white hover:text-red-600 text-white rounded-full py-1.5 px-3"
-                    to="/news"
-                  >
-                    Lees meer
-                  </router-link>
-                </div>
-              </div>
-            </div>
+            <NewsCard :story="story" />
           </template>
         </NewsList>
       </div>
@@ -73,7 +35,6 @@
 </template>
 
 <script setup lang="ts">
-// eslint-disable-next-line import/no-absolute-path
-import newsImage from '/news.jpg';
 import NewsList from '@root/components/NewsList.vue';
+import NewsCard from '@root/components/NewsCard.vue';
 </script>
