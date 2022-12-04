68ea859021c51f8accae3f7c67d96ee7d0929fee
fbraem <franky.braem@gmail.com>
2022-12-04 21:54:15 +0100

    Add coaches

--- apps/portal/src/pages/TrainingsPage.vue
+++ apps/portal/src/pages/TrainingsPage.vue
@@ -42,9 +42,9 @@
   </section>
   <div id="article" />
   <router-view />
-  <section class="bg-zinc-50 py-24">
+  <section class="py-24">
     <div class="container mx-auto">
-      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 divide-y md:divide-y-0 md:divide-x divide-gray-300">
+      <div class="grid grid-cols-1 md:grid-cols-2 gap-4 divide-y divide-y-reverse md:divide-y-0 md:divide-x divide-gray-300">
         <div class="p-4">
           <h2 class="text-center text-4xl mb-10">
             Belangrijk Nieuws
@@ -61,7 +61,7 @@
             </NewsList>
           </div>
         </div>
-        <div class="p-4">
+        <div class="p-4 order-first md:order-last">
           <h2 class="text-center text-4xl mb-2">
             Agenda
           </h2>
@@ -70,6 +70,39 @@
           </p>
           <div>
             <TrainingWeek />
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <section class="bg-zinc-50 py-24">
+    <div class="container mx-auto">
+      <h2 class="text-center text-4xl mb-10">
+        Onze Coaches
+      </h2>
+      <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4">
+        <div
+          v-for="coach in coaches"
+          :key="coach.id"
+          class="bg-white px-3 py-2"
+        >
+          <div
+            class="h-80 bg-contain bg-center bg-no-repeat"
+            :style="{ 'background-image': `url(${noAvatarUrl}` }"
+          />
+          <div class="pt-3 text-center">
+            <h3 class="text-2xl font-semibold">
+              {{ coach.name }}
+            </h3>
+            <h4
+              v-if="coach.diploma"
+              class="text-gray-400"
+            >
+              {{ coach.diploma }}
+            </h4>
+            <p class="text-sm text-gray-600 mb-4">
+              {{ coach.bio }}
+            </p>
           </div>
         </div>
       </div>
@@ -89,12 +122,19 @@ import { computed } from 'vue';
 import { useApplicationStore } from '@root/stores/applicationStore';
 import { useRouter } from 'vue-router';
 import NewsArticleSummary from '@root/components/NewsArticleSummary.vue';
+import { useCoachStore } from '@root/stores/coachStore';
 
+// Coaches
+// eslint-disable-next-line import/no-absolute-path
+import noAvatarUrl from '/no_avatar.png';
+
+// Application
 const applicationStore = useApplicationStore();
 applicationStore.setActiveApplication('trainings');
 const application = computed(() => applicationStore.activeApplication);
 const applicationId = computed(() => application.value?.id);
 
+// Articles
 const articleStore = useArticleStore();
 articleStore.load({ application: applicationId });
 
@@ -111,4 +151,8 @@ const gotoArticle = async(id: string) => {
     el.scrollIntoView({ block: 'center' });
   }
 };
+
+const coachStore = useCoachStore();
+const coaches = computed(() => coachStore.coaches);
+coachStore.load();
 </script>
